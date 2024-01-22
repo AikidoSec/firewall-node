9002f182942fba52529bac936e1213afe9b9b771
Hans Ott <hansott@hotmail.be>
2024-01-22 13:24:32 +0100

    Add sample app that uses mongodb to list posts

--- apps/express-mongodb/app.js
+++ apps/express-mongodb/app.js
@@ -1,12 +1,55 @@
 require("../../dist"); // Normally you would use require("aikido");
 
 const express = require("express");
+const asyncHandler = require("express-async-handler");
 const app = express();
+const { MongoClient } = require("mongodb");
+const { Posts, Post } = require("./posts");
+const { escape } = require("./escape");
+const morgan = require("morgan");
+const url = "mongodb://root:password@localhost:27017";
+const client = new MongoClient(url, { family: 4 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const posts = new Posts(client);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+app.use(morgan("tiny"));
+
+app.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const homePagePosts = await posts.all();
+
+    res.send(`
+      <html>
+        <body>
+          <ul>
+            ${homePagePosts.map((post) => `<li>${escape(post.getTitle())}</li>`).join("\n")}
+          </ul>
+          <form action="/" method="POST">
+            <input type="text" name="title" />
+            <input type="submit" value="Submit" />
+          </form>
+        </body>
+      </html>
+    `);
+  })
+);
+
+app.post(
+  "/",
+  express.urlencoded({ extended: false }),
+  asyncHandler(async (req, res) => {
+    const post = new Post(req.body.title, new Date());
+    await posts.persist(post);
+    res.redirect("/");
+  })
+);
+
+async function main() {
+  await client.connect();
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
+}
+
+main();
---
+++ apps/express-mongodb/posts.js
@@ -0,0 +1,40 @@
+class Post {
+  constructor(title, createdAt) {
+    this._title = title;
+    this._createdAt = createdAt;
+  }
+
+  getTitle() {
+    return this._title;
+  }
+
+  getCreatedAt() {
+    return this._createdAt;
+  }
+}
+
+class Posts {
+  constructor(mongo) {
+    this.db = mongo.db("app");
+  }
+
+  async all() {
+    const collection = this.db.collection("posts");
+    const posts = await collection.find({}).toArray();
+
+    return posts.map((post) => new Post(post.title, post.createdAt));
+  }
+
+  async persist(post) {
+    const collection = this.db.collection("posts");
+    await collection.insertOne({
+      title: post.getTitle(),
+      createdAt: post.getCreatedAt(),
+    });
+  }
+}
+
+module.exports = {
+  Posts,
+  Post,
+};
