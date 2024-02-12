183e6c95dab6862617e177cea210b3b2cfcecc6f
Hans Ott <hansott@hotmail.be>
2024-02-12 12:31:52 +0100

    Add tests for Agent

--- library/src/agent/Agent.test.ts
+++ library/src/agent/Agent.test.ts
@@ -64,5 +64,148 @@ t.test("it sends install event once", async (t) => {
     },
   ]);
 
+  // Stop setInterval from heartbeat
   agent.stop();
+});
+
+t.test("when prevent prototype pollution is enabled", async (t) => {
+  const logger = new LoggerNoop();
+  const api = new APIForTesting();
+  const token = new Token("123");
+  const agent = new Agent(
+    true,
+    logger,
+    api,
+    token,
+    [],
+    new IDGeneratorFixed("id"),
+    false
+  );
+  agent.start();
+  // This is a private property, not a good practice to write test like this :)
+  t.match(agent, { info: { preventedPrototypePollution: false } });
+  agent.onPrototypePollutionPrevented();
+  t.match(agent, { info: { preventedPrototypePollution: true } });
+  agent.stop();
+});
+
+t.test("it does not start interval in serverless mode", async () => {
+  const logger = new LoggerNoop();
+  const api = new APIForTesting();
+  const token = new Token("123");
+  const agent = new Agent(
+    true,
+    logger,
+    api,
+    token,
+    [],
+    new IDGeneratorFixed("id"),
+    true
+  );
+
+  // This would otherwise keep the process running
+  agent.start();
+});
+
+t.test("it keeps track of stats", async () => {
+  const logger = new LoggerNoop();
+  const api = new APIForTesting();
+  const token = new Token("123");
+  const agent = new Agent(
+    true,
+    logger,
+    api,
+    token,
+    [],
+    new IDGeneratorFixed("id"),
+    true
+  );
+
+  agent.start();
+  agent.onInspectedCall({
+    module: "mongodb",
+    withoutContext: false,
+    detectedAttack: false,
+  });
+
+  // This is a private property, not a good practice to write test like this :)
+  t.match(agent, {
+    stats: {
+      mongodb: {
+        blocked: 0,
+        allowed: 1,
+        withoutContext: 0,
+        total: 1,
+      },
+    },
+  });
+
+  agent.onInspectedCall({
+    module: "mongodb",
+    withoutContext: true,
+    detectedAttack: false,
+  });
+
+  t.match(agent, {
+    stats: {
+      mongodb: {
+        blocked: 0,
+        allowed: 2,
+        withoutContext: 1,
+        total: 2,
+      },
+    },
+  });
+
+  agent.onInspectedCall({
+    module: "mongodb",
+    withoutContext: false,
+    detectedAttack: true,
+  });
+
+  t.match(agent, {
+    stats: {
+      mongodb: {
+        blocked: 1,
+        allowed: 2,
+        withoutContext: 1,
+        total: 3,
+      },
+    },
+  });
+});
+
+t.test("it keeps tracks of stats in dry mode", async () => {
+  const logger = new LoggerNoop();
+  const api = new APIForTesting();
+  const token = new Token("123");
+  const agent = new Agent(
+    false,
+    logger,
+    api,
+    token,
+    [],
+    new IDGeneratorFixed("id"),
+    true
+  );
+
+  agent.start();
+
+  agent.onInspectedCall({
+    module: "mongodb",
+    withoutContext: false,
+    detectedAttack: true,
+  });
+
+  // This is a private property, not a good practice to write test like this :)
+  t.match(agent, {
+    stats: {
+      mongodb: {
+        blocked: 0,
+        allowed: 1,
+        withoutContext: 0,
+        total: 1,
+      },
+    },
+  });
 });
