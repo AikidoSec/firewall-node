855f5dc5f744865d7d47a62c6655cad65354f83d
BitterPanda <gitlab+dev@woutfeys.be>
2024-02-19 16:45:57 +0100

    Change wrap to massWrap

--- library/src/sinks/Postgres.ts
+++ library/src/sinks/Postgres.ts
@@ -1,7 +1,7 @@
 import { Wrapper } from "../agent/Wrapper";
 import { Client } from "pg";
 import { Hook } from "require-in-the-middle";
-import { wrap } from "shimmer";
+import { massWrap } from "shimmer";
 import { Agent } from "../agent/Agent";
 import { getInstance } from "../agent/AgentSingleton";
 import { Context, getContext } from "../agent/Context";
@@ -13,10 +13,10 @@ export class Postgres implements Wrapper {
     private wrapQueryFunction(exports:unknown) {
         const that = this;
 
-        wrap(
+        massWrap(
         // @ts-expect-error This is magic that TypeScript doesn't understand
-        exports.Client.prototype,
-        "query",
+        [exports.Client.prototype, exports.Pool.prototype],
+        ["query"],
         function wrapQueryFunction(original) {
             return function safeQueryFunction(this: Client ) {
                 const agent = getInstance();
