07945ed3bb6723ebcd194c47dbb377d0147ffd06
BitterPanda <gitlab+dev@woutfeys.be>
2024-02-19 18:23:18 +0100

    Create a new wrapper class

--- library/src/agent/Wrapper.ts
+++ library/src/agent/Wrapper.ts
@@ -1,3 +1,29 @@
-export interface Wrapper {
-  wrap(): void;
+import { Hook } from "require-in-the-middle";
+import { massWrap } from "shimmer";
+
+export class Wrapper {
+  private packageName;
+  constructor(
+    packageName: string, 
+    middleware:(...args:any[]) => any[],
+    modules:object[],
+    functionNames: never[]
+  ) {
+    this.packageName = packageName;
+  }
+  private wrapFunction(exports:unknown) {
+    // @ts-expect-error This is magic that TypeScript doesn't understand
+    massWrap(modules, functionNames, function wrapFunction(original:any) {
+      return function insertMiddleware(this:any) {
+        return original.apply(this, arguments);
+      }
+    })
+  }
+  private onModuleRequired<T>(exports: T): T {
+    this.wrapFunction(exports);
+    return exports;
+  }
+  public activate() {
+      new Hook([this.packageName], this.onModuleRequired.bind(this));
+  }
 }
