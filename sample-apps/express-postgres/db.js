4dadc67208f9abbac6f83a70ee8ed7574ccc0505
BitterPanda <gitlab+dev@woutfeys.be>
2024-02-19 11:24:26 +0100

    Add the client and pg code to db.js

--- sample-apps/express-postgres/db.js
+++ sample-apps/express-postgres/db.js
@@ -1,4 +1,24 @@
-async function initDb(client) {
+const { Client } = require('pg');
+let client;
+
+async function connectToPostgresDB() {
+    // Normally you'd use environment variables for this
+    // These values were also referenced in the docker-compose.yml
+    client = new Client({
+        user: 'root',
+        host: '127.0.0.1',
+        database: 'main_db',
+        password: 'password',
+        port: 27016,
+    });
+
+    await client.connect();
+    console.log(await client.query('SELECT NOW()'));
+
+    await initDb();
+}
+
+async function initDb() {
     // This creates the cats table
     await client.query(`
     CREATE TABLE IF NOT EXISTS cats (
@@ -7,12 +27,14 @@ async function initDb(client) {
     `);
 }
 
-async function insertCatIntoTable(petname, client) {
+async function insertCatIntoTable(petname) {
     // This makes your database vulnerable to SQL Injections! vvv
     await client.query(`INSERT INTO cats(petname) VALUES ('${petname}');`)
 }
 
-async function getAllCats(client) {
+async function getAllCats() {
     // This function returns all cats in the db
     const cats = await client.query("SELECT petname FROM cats;")   
 }
+
+module.exports = {connectToPostgresDB, insertCatIntoTable, getAllCats};
