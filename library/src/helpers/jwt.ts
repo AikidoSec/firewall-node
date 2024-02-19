00910437f9557eac4c67e25d766a74729ce0e4d4
BitterPanda <gitlab+dev@woutfeys.be>
2024-02-19 19:06:15 +0100

    Finish jwt.ts

--- library/src/helpers/jwt.ts
+++ library/src/helpers/jwt.ts
@@ -1,12 +1,14 @@
 /**
  * The JWT module only exports one function : tryDecodeAsJWT
- * @module JWT
+ * @module helpers/JWT
  */
 
 /**
- * 
- * @param jwt 
- * @returns 
+ * This function tries to decode your input as if it is a JSON Web Token, if it fails to do so it returns {jwt: false}.
+ * @param jwt A (possible) JWT
+ * @returns The JWT if it's valid, otherwise it returns false
+ * @example
+ * tryDecodeAsJWT("invalid"); // Returns {jwt: false}
  */
 export function tryDecodeAsJWT(
   jwt: string
