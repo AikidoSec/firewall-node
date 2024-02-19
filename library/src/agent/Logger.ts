180a1863b4b6dd0af41af901683809553f8028ad
BitterPanda <gitlab+dev@woutfeys.be>
2024-02-19 14:04:38 +0100

    Add documentation to Logger.ts

--- library/src/agent/Logger.ts
+++ library/src/agent/Logger.ts
@@ -1,8 +1,22 @@
+/**
+ * Exports 2 classes related to logging
+ * + Type definition Logger
+ * @module Logger
+ */
+
 export interface Logger {
   log(message: string): void;
 }
 
+/**
+ * A console class that has the log function
+ * @class LoggerConsole
+ */
 export class LoggerConsole {
+  /**
+   * Creates a terminal log with the "AIKIDO: " affix.
+   * @param message Message to be logged
+   */
   log(message: string) {
     console.log(`AIKIDO: ${message}`);
   }
