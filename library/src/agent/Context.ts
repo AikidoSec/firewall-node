973940b4557b4c38683032fac43196e60e86fedb
BitterPanda <gitlab+dev@woutfeys.be>
2024-02-19 14:37:13 +0100

    Trying to add some documentation to Context.ts

--- library/src/agent/Context.ts
+++ library/src/agent/Context.ts
@@ -1,6 +1,14 @@
 import { AsyncLocalStorage } from "node:async_hooks";
 import type { ParsedQs } from "qs";
 
+/**
+ * @prop url This is the requested URL
+ * @prop method This is the HTTP Method used to request the URL
+ * @prop query These are the URL Query parameters (e.g. example.com?param1=value1)
+ * @prop headers The HTTP headers accompanying the request
+ * @prop The remote address 
+ * @interface
+ */
 export type Context = {
   url: string | undefined;
   method: string;
@@ -13,10 +21,20 @@ export type Context = {
 
 const requestContext = new AsyncLocalStorage<Context>();
 
+/**
+ * This function gives you the {@link Context} stored in asynchronous local storage
+ * @returns the stored context in asynchronous local storage
+ */
 export function getContext() {
   return requestContext.getStore();
 }
 
+/**
+ * 
+ * @param context The context you want to set ({@link Context})
+ * @param fn 
+ * @returns 
+ */
 export function runWithContext<T>(context: Context, fn: () => T) {
   return requestContext.run(context, fn);
 }
