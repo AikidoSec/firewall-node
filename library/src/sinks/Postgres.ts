643bc08568bf0f7b9d3432b40d8f3d8c10a99c36
BitterPanda <gitlab+dev@woutfeys.be>
2024-02-19 16:16:59 +0100

    Check if agent exists or not

--- library/src/sinks/Postgres.ts
+++ library/src/sinks/Postgres.ts
@@ -1,4 +1,5 @@
 import { Wrapper } from "../agent/Wrapper";
+import { Client } from "pg";
 import { Hook } from "require-in-the-middle";
 import { wrap } from "shimmer";
 import { Agent } from "../agent/Agent";
@@ -6,7 +7,22 @@ import { getInstance } from "../agent/AgentSingleton";
 
 export class Postgres implements Wrapper {
     private wrapQueryFunction(exports:unknown) {
-        
+        const that = this;
+
+        wrap(
+        // @ts-expect-error This is magic that TypeScript doesn't understand
+        exports.Client.prototype,
+        "query",
+        function wrapQueryFunction(original) {
+            return function safeQueryFunction(this: Client ) {
+                const agent = getInstance();
+
+                if (!agent) {
+                    return original.apply(this, arguments);
+                }
+            }
+        }
+        )
     }
 
     private onModuleRequired<T>(exports: T): T {
