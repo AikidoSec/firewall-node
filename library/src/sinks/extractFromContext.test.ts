09da5605937259fd1fabb4540a138701031f5cd3
BitterPanda <gitlab+dev@woutfeys.be>
2024-02-20 11:09:30 +0100

    Write tests for the extract function

--- library/src/sinks/extractFromContext.test.ts
+++ library/src/sinks/extractFromContext.test.ts
@@ -1,8 +1,12 @@
 import * as t from "tap";
-import {
-  extractFromContext,
-  extractFromQuery,
-  extractFromHeaders,
-  extractFromCookies,
-  extractFromBody,
-} from "./extractFromContext";
+import { extract } from "./extractFromContext";
+
+t.test("Can extract() extract Query objects", async () => {
+  t.same(extract({ age: { $gt: "21" } }), ["age", "$gt", "21"]);
+  t.same(extract({ title: { $ne: "null" } }), ["title", "$ne", "null"]);
+  t.same(extract({ age: "whaat", user_input: ["whaat", "dangerous"]}), ["user_input", "age", "whaat", "dangerous"])
+});
+
+t.test("Can extract() extract cookie objects", async () => {
+  t.same(extract({session: "ABC", session2: "DEF"}), ["session2", "session", "ABC", "DEF"])
+})
