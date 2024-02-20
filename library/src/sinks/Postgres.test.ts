0d6cfe339ff3d57c5b66e254945ea78fd800c155
BitterPanda <gitlab+dev@woutfeys.be>
2024-02-20 13:21:32 +0100

    Update tests and regex

--- library/src/sinks/Postgres.test.ts
+++ library/src/sinks/Postgres.test.ts
@@ -2,19 +2,19 @@ import * as t from "tap";
 import {inputPossibleSql} from "./Postgres";
 
 t.test("Check if SQL commands are flagged", async () => {
-    t.ok(inputPossibleSql("Roses are red insErts are blue"));
-    t.ok(inputPossibleSql("Roses are red cREATES are blue"));
+    t.ok(inputPossibleSql("Roses are red insErt are blue"));
+    t.ok(inputPossibleSql("Roses are red cREATE are blue"));
     t.ok(inputPossibleSql("Roses are red drop are blue"));
     t.ok(inputPossibleSql("Roses are red updatE are blue"));
     t.ok(inputPossibleSql("Roses are red SELECT are blue"));
     t.ok(inputPossibleSql("Roses are red dataBASE are blue"));
-    t.ok(inputPossibleSql("Roses are red deletes are blue"));
+    t.notOk(inputPossibleSql("Roses are reddelete are blue"));
     t.ok(inputPossibleSql("Roses are red alter are blue"));
     t.ok(inputPossibleSql("Roses are red grant are blue"));
     t.ok(inputPossibleSql("Roses are red savepoint are blue"));
     t.ok(inputPossibleSql("Roses are red commit are blue"));
-    t.ok(inputPossibleSql("Roses are red rollbacks are blue"));
-    t.ok(inputPossibleSql("Roses are red truncates are blue"));
+    t.notOk(inputPossibleSql("Roses are red rollbacks are blue"));
+    t.notOk(inputPossibleSql("Roses are red truncates are blue"));
 
     t.notOk(inputPossibleSql("Roses are red WHEREis blue"));
     t.notOk(inputPossibleSql("Roses are redFROM is blue"));
