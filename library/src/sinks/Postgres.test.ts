3ebb311b7177737ab93413999ecf89e248550077
BitterPanda <gitlab+dev@woutfeys.be>
2024-02-20 12:44:44 +0100

    Add test cases and activate function

--- library/src/sinks/Postgres.test.ts
+++ library/src/sinks/Postgres.test.ts
@@ -1,2 +1,22 @@
 import * as t from "tap";
 import {inputPossibleSql} from "./Postgres";
+
+t.test("Check if SQL commands are flagged", async () => {
+    t.ok(inputPossibleSql("Roses are red insErts are blue"));
+    t.ok(inputPossibleSql("Roses are red cREATES are blue"));
+    t.ok(inputPossibleSql("Roses are red drop are blue"));
+    t.ok(inputPossibleSql("Roses are red updatE are blue"));
+    t.ok(inputPossibleSql("Roses are red SELECT are blue"));
+    t.ok(inputPossibleSql("Roses are red dataBASE are blue"));
+    t.ok(inputPossibleSql("Roses are red deletes are blue"));
+    t.ok(inputPossibleSql("Roses are red alter are blue"));
+    t.ok(inputPossibleSql("Roses are red grant are blue"));
+    t.ok(inputPossibleSql("Roses are red savepoint are blue"));
+    t.ok(inputPossibleSql("Roses are red commit are blue"));
+    t.ok(inputPossibleSql("Roses are red rollbacks are blue"));
+    t.ok(inputPossibleSql("Roses are red truncates are blue"));
+
+    t.notOk(inputPossibleSql("Roses are red WHEREis blue"));
+    t.notOk(inputPossibleSql("Roses are redFROM is blue"));
+    t.notOk(inputPossibleSql("Roses are red ORis isAND"));
+})
