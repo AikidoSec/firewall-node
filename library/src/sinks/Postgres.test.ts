4369d3672d88da4fd9668b661a11642c504c8b73
BitterPanda <gitlab+dev@woutfeys.be>
2024-02-20 15:30:07 +0100

    Move tests to arrays

--- library/src/sinks/Postgres.test.ts
+++ library/src/sinks/Postgres.test.ts
@@ -1,89 +1,73 @@
 import * as t from "tap";
 import { inputPossibleSql, sqlContainsInput } from "./Postgres";
+const BAD_SQL_COMMANDS = [
+  // Check for SQL Commands like : INSERT or DROP
+  "Roses are red insErt are blue",
+  "Roses are red cREATE are blue",
+  "Roses are red drop are blue",
+  "Roses are red updatE are blue",
+  "Roses are red SELECT are blue",
+  "Roses are red dataBASE are blue",
+  "Roses are reddelete are blue",
+  "Roses are red alter are blue",
+  "Roses are red grant are blue",
+  "Roses are red savepoint are blue",
+  "Roses are red commit are blue",
+  "Roses are red or blue",
+  "Roses are red and lovely",
+  // https://github.com/payloadbox/sql-injection-payload-list/blob/master/Intruder/payloads-sql-blind/MSSQL/payloads-sql-blind-MSSQL-INSERT.txt
+  `"),NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL)%20waitfor%20delay%20'0:0:20'%20--`,
+  `',NULL,NULL,NULL,NULL,NULL,NULL)%20waitfor%20delay%20'0:0:20'%20--`,
+  `',NULL,NULL,NULL,NULL,NULL)%20waitfor%20delay%20'0:0:20'%20--`,
+  `)%20waitfor%20delay%20'0:0:20'%20/*`,
+  `))%20waitfor%20delay%20'0:0:20'%20--`,
+  // https://github.com/payloadbox/sql-injection-payload-list/blob/master/Intruder/detect/MySQL/MySQL.txt
+  `1'1`,
+  `1 exec sp_ (or exec xp_)`,
+  `1 and 1=1`,
+  `1' and 1=(select count(*) from tablenames); --`,
+  `1 or 1=1`,
+  `1or1=1`,
+  `fake@ema'or'il.nl'='il.nl`,
+  `1'or'1'='1`,
+  // https://github.com/payloadbox/sql-injection-payload-list/blob/master/Intruder/exploit/Auth_Bypass.txt
+  `'-'`,
+  `")) or (("x"))=(("x`,
+  `admin' or '1'='1'#`,
+  `' AND 1=0 UNION ALL SELECT '', '81dc9bdb52d04dc20036dbd8313ed055`,
+  `') or ('a'='a and hi") or ("a"="a`,
+  `" or "1"="1`,
+  `' UNION ALL SELECT system_user(),user();#`,
+  `admin' and substring(password/text(),1,1)='7`,
+  `' or 1=1 limit 1 -- -+`,
+  ` or 1=1–`,
+  ` or 1=1`,
+  // Test some special characters
+  "steve@yahoo.com",
+  "I'm writting you",
+  "This is not ok--",
+  "Termin;ate",
+];
+const GOOD_SQL_COMMANDS = [
+  // Check for SQL Commands with allowed characters
+  "Roses are red rollbacks are blue",
+  "Roses are red truncates are blue",
+  "Roses are red WHEREis blue",
+  "Roses are redFROM is blue",
+  "Roses are red ORis isAND",
+  // Check for some general statements
+  `abcdefghijklmnop@hotmail.com`,
+  `roses are red violets are blue!`,
+  `1 is cool 2 is nice 3 thats thrice.`,
+];
 
-t.test("Check if SQL commands are flagged", async () => {
-  t.ok(inputPossibleSql("Roses are red insErt are blue"));
-  t.ok(inputPossibleSql("Roses are red cREATE are blue"));
-  t.ok(inputPossibleSql("Roses are red drop are blue"));
-  t.ok(inputPossibleSql("Roses are red updatE are blue"));
-  t.ok(inputPossibleSql("Roses are red SELECT are blue"));
-  t.ok(inputPossibleSql("Roses are red dataBASE are blue"));
-  t.notOk(inputPossibleSql("Roses are reddelete are blue"));
-  t.ok(inputPossibleSql("Roses are red alter are blue"));
-  t.ok(inputPossibleSql("Roses are red grant are blue"));
-  t.ok(inputPossibleSql("Roses are red savepoint are blue"));
-  t.ok(inputPossibleSql("Roses are red commit are blue"));
-  t.notOk(inputPossibleSql("Roses are red rollbacks are blue"));
-  t.notOk(inputPossibleSql("Roses are red truncates are blue"));
-
-  t.notOk(inputPossibleSql("Roses are red WHEREis blue"));
-  t.notOk(inputPossibleSql("Roses are redFROM is blue"));
-  t.notOk(inputPossibleSql("Roses are red ORis isAND"));
-});
-
-// https://github.com/payloadbox/sql-injection-payload-list/blob/master/Intruder/payloads-sql-blind/MSSQL/payloads-sql-blind-MSSQL-INSERT.txt
-t.test("Check for statements from MSSQL-INSERT.txt", async () => {
-  t.ok(
-    inputPossibleSql(
-      `"),NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL)%20waitfor%20delay%20'0:0:20'%20--`
-    )
-  );
-  t.ok(
-    inputPossibleSql(
-      `',NULL,NULL,NULL,NULL,NULL,NULL)%20waitfor%20delay%20'0:0:20'%20--`
-    )
-  );
-  t.ok(
-    inputPossibleSql(
-      `',NULL,NULL,NULL,NULL,NULL)%20waitfor%20delay%20'0:0:20'%20--`
-    )
-  );
-  t.ok(inputPossibleSql(`)%20waitfor%20delay%20'0:0:20'%20/*`));
-  t.ok(inputPossibleSql(`))%20waitfor%20delay%20'0:0:20'%20--`));
-});
-
-// https://github.com/payloadbox/sql-injection-payload-list/blob/master/Intruder/detect/MySQL/MySQL.txt
-t.test("Check for statements from MySQL.txt", async () => {
-  t.ok(inputPossibleSql(`1'1`));
-  t.ok(inputPossibleSql(`1 exec sp_ (or exec xp_)`));
-  t.ok(inputPossibleSql(`1 and 1=1`));
-  t.ok(inputPossibleSql(`1' and 1=(select count(*) from tablenames); --`));
-  t.ok(inputPossibleSql(`1 or 1=1`));
-  t.ok(inputPossibleSql(`1or1=1`));
-  t.ok(inputPossibleSql(`fake@ema'or'il.nl'='il.nl`));
-  t.ok(inputPossibleSql(`1'or'1'='1`));
-});
-
-// https://github.com/payloadbox/sql-injection-payload-list/blob/master/Intruder/exploit/Auth_Bypass.txt
-t.test("Check for statements from Auth_Bypass.txt", async () => {
-  t.ok(inputPossibleSql(`'-'`));
-  t.ok(inputPossibleSql(`")) or (("x"))=(("x`));
-  t.ok(inputPossibleSql(`admin' or '1'='1'#`));
-  t.ok(
-    inputPossibleSql(
-      `' AND 1=0 UNION ALL SELECT '', '81dc9bdb52d04dc20036dbd8313ed055`
-    )
-  );
-  t.ok(inputPossibleSql(`') or ('a'='a and hi") or ("a"="a`));
-  t.ok(inputPossibleSql(`" or "1"="1`));
-  t.ok(inputPossibleSql(`' UNION ALL SELECT system_user(),user();#`));
-  t.ok(inputPossibleSql(`admin' and substring(password/text(),1,1)='7`));
-  t.ok(inputPossibleSql(`' or 1=1 limit 1 -- -+`));
-  t.ok(inputPossibleSql(` or 1=1–`));
-  t.ok(inputPossibleSql(` or 1=1`));
-});
-
-t.test("Check for some statements that are allowed", async () => {
-  t.notOk(inputPossibleSql(`abcdefghijklmnop@hotmail.com`));
-  t.notOk(inputPossibleSql(`roses are red violets are blue!`));
-  t.notOk(inputPossibleSql(`1 is cool 2 is nice 3 thats thrice.`));
-});
-
-t.test("Test some special characters", async () => {
-  t.ok(inputPossibleSql("Termin;ate"));
-  t.notOk(inputPossibleSql("steve@yahoo.com"));
-  t.ok(inputPossibleSql("I'm writting you"));
-  t.ok(inputPossibleSql("This is not ok--"));
+t.test("Test the inputPossibleSql() function", async () => {
+  for (const sql of BAD_SQL_COMMANDS) {
+    t.ok(inputPossibleSql(sql), sql);
+  }
+  for (const sql of GOOD_SQL_COMMANDS) {
+    t.notOk(inputPossibleSql(sql), sql);
+  }
 });
 
 t.test("Test the sqlContainsInput() function", async () => {
