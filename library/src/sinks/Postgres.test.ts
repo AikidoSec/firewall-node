e586d254d5d6d177e5cd85e15e6d4eab6ca6a946
BitterPanda <gitlab+dev@woutfeys.be>
2024-02-20 13:47:14 +0100

    Actually execute the inputPossibleSql function...

--- library/src/sinks/Postgres.test.ts
+++ library/src/sinks/Postgres.test.ts
@@ -23,36 +23,37 @@ t.test("Check if SQL commands are flagged", async () => {
 
 // https://github.com/payloadbox/sql-injection-payload-list/blob/master/Intruder/payloads-sql-blind/MSSQL/payloads-sql-blind-MSSQL-INSERT.txt
 t.test("Check for statements from MSSQL-INSERT.txt", async () => {
-    t.ok(`"),NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL)%20waitfor%20delay%20'0:0:20'%20--`);
-    t.ok(`',NULL,NULL,NULL,NULL,NULL,NULL)%20waitfor%20delay%20'0:0:20'%20--`);
-    t.ok(`',NULL,NULL,NULL,NULL,NULL)%20waitfor%20delay%20'0:0:20'%20--`);
-    t.ok(`)%20waitfor%20delay%20'0:0:20'%20/*`);
-    t.ok(`))%20waitfor%20delay%20'0:0:20'%20--`);
+    t.ok(inputPossibleSql(`"),NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL)%20waitfor%20delay%20'0:0:20'%20--`));
+    t.ok(inputPossibleSql(`',NULL,NULL,NULL,NULL,NULL,NULL)%20waitfor%20delay%20'0:0:20'%20--`));
+    t.ok(inputPossibleSql(`',NULL,NULL,NULL,NULL,NULL)%20waitfor%20delay%20'0:0:20'%20--`));
+    t.ok(inputPossibleSql(`)%20waitfor%20delay%20'0:0:20'%20/*`));
+    t.ok(inputPossibleSql(`))%20waitfor%20delay%20'0:0:20'%20--`));
 });
 
 // https://github.com/payloadbox/sql-injection-payload-list/blob/master/Intruder/detect/MySQL/MySQL.txt
 t.test("Check for statements from MySQL.txt", async() => {
-    t.ok(`1'1`);
-    t.ok(`1 exec sp_ (or exec xp_)`);
-    t.ok(`1 and 1=1`);
-    t.ok(`1' and 1=(select count(*) from tablenames); --`);
-    t.ok(`1 or 1=1`);
-    t.ok(`1or1=1`);
-    t.ok(`fake@ema'or'il.nl'='il.nl`);
-    t.ok(`1'or'1'='1`)
+    t.ok(inputPossibleSql(`1'1`));
+    t.ok(inputPossibleSql(`1 exec sp_ (or exec xp_)`));
+    t.ok(inputPossibleSql(`1 and 1=1`));
+    t.ok(inputPossibleSql(`1' and 1=(select count(*) from tablenames); --`));
+    t.ok(inputPossibleSql(`1 or 1=1`));
+    t.ok(inputPossibleSql(`1or1=1`));
+    t.ok(inputPossibleSql(`fake@ema'or'il.nl'='il.nl`));
+    t.ok(inputPossibleSql(`1'or'1'='1`));
 })
 
 // https://github.com/payloadbox/sql-injection-payload-list/blob/master/Intruder/exploit/Auth_Bypass.txt
 t.test("Check for statements from Auth_Bypass.txt", async () => {
-    t.ok(`'-'`);
-    t.ok(`")) or (("x"))=(("x`);
-    t.ok(`admin' or '1'='1'#`);
-    t.ok(`' AND 1=0 UNION ALL SELECT '', '81dc9bdb52d04dc20036dbd8313ed055`);
-    t.ok(`') or ('a'='a and hi") or ("a"="a`);
-    t.ok(`" or "1"="1`);
-    t.ok(`' UNION ALL SELECT system_user(),user();#`);
-    t.ok(`admin' and substring(password/text(),1,1)='7`);
-    t.ok(`' or 1=1 limit 1 -- -+`);
-    t.ok(` or 1=1–`);
-    t.ok(` or 1=1`)
+    t.ok(inputPossibleSql(`'-'`));
+    t.ok(inputPossibleSql(`")) or (("x"))=(("x`));
+    t.ok(inputPossibleSql(`admin' or '1'='1'#`));
+    t.ok(inputPossibleSql(`' AND 1=0 UNION ALL SELECT '', '81dc9bdb52d04dc20036dbd8313ed055`));
+    t.ok(inputPossibleSql(`') or ('a'='a and hi") or ("a"="a`));
+    t.ok(inputPossibleSql(`" or "1"="1`));
+    t.ok(inputPossibleSql(`' UNION ALL SELECT system_user(),user();#`));
+    t.ok(inputPossibleSql(`admin' and substring(password/text(),1,1)='7`));
+    t.ok(inputPossibleSql(`' or 1=1 limit 1 -- -+`));
+    t.ok(inputPossibleSql(` or 1=1–`));
+    t.ok(inputPossibleSql(` or 1=1`));
+});
 });
