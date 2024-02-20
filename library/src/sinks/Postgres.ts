25d7eb2434dfda1a1bb20165217fc2c2854a7810
BitterPanda <gitlab+dev@woutfeys.be>
2024-02-20 07:52:34 +0100

    Make sure code respects prettier guidelines

--- library/src/sinks/Postgres.ts
+++ library/src/sinks/Postgres.ts
@@ -7,54 +7,54 @@ import { getInstance } from "../agent/AgentSingleton";
 import { Context, getContext } from "../agent/Context";
 
 export class Postgres implements Wrapper {
-    private checkForSqlInjection(sqlStatement:string, request:Context) {
-        // Currently, do nothing : Still needs to be implemented
-    };
-    private wrapQueryFunction(exports:unknown) {
-        const that = this;
+  private checkForSqlInjection(sqlStatement: string, request: Context) {
+    // Currently, do nothing : Still needs to be implemented
+  }
+  private wrapQueryFunction(exports: unknown) {
+    const that = this;
 
-        massWrap(
-        // @ts-expect-error This is magic that TypeScript doesn't understand
-        [exports.Client.prototype, exports.Pool.prototype],
-        ["query"],
-        function wrapQueryFunction(original) {
-            return function safeQueryFunction(this: Client ) {
-                const agent = getInstance();
-                if (!agent) {
-                    return original.apply(this, arguments);
-                }
+    massWrap(
+      // @ts-expect-error This is magic that TypeScript doesn't understand
+      [exports.Client.prototype, exports.Pool.prototype],
+      ["query"],
+      function wrapQueryFunction(original) {
+        return function safeQueryFunction(this: Client) {
+          const agent = getInstance();
+          if (!agent) {
+            return original.apply(this, arguments);
+          }
 
-                const request = getContext();
-                if (!request) {
-                    agent.onInspectedCall({
-                        module: "postgres",
-                        withoutContext: true,
-                        detectedAttack: false,
-                    });
+          const request = getContext();
+          if (!request) {
+            agent.onInspectedCall({
+              module: "postgres",
+              withoutContext: true,
+              detectedAttack: false,
+            });
 
-                    return original.apply(this, arguments);
-                }
+            return original.apply(this, arguments);
+          }
 
-                let querystring:string = arguments[0];
-                if(typeof querystring !== "string") {
-                    // The query is not a string, not much to do here
-                    return original.apply(this, arguments);
-                }
+          let querystring: string = arguments[0];
+          if (typeof querystring !== "string") {
+            // The query is not a string, not much to do here
+            return original.apply(this, arguments);
+          }
 
-                that.checkForSqlInjection(querystring, request);
+          that.checkForSqlInjection(querystring, request);
 
-                return original.apply(this, arguments);
-            }
-        }
-        )
-    }
+          return original.apply(this, arguments);
+        };
+      }
+    );
+  }
 
-    private onModuleRequired<T>(exports: T): T {
-        this.wrapQueryFunction(exports);
-        return exports;
-    }
-    
-    wrap() {
-        new Hook(["pg"], this.onModuleRequired.bind(this));
-    }
+  private onModuleRequired<T>(exports: T): T {
+    this.wrapQueryFunction(exports);
+    return exports;
+  }
+
+  wrap() {
+    new Hook(["pg"], this.onModuleRequired.bind(this));
+  }
 }
