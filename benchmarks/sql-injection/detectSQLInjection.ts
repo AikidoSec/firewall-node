a97b3afa5c1cead89fd3bca9520f95cefc2cbdbb
BitterPanda <gitlab+dev@woutfeys.be>
2024-02-20 16:43:21 +0100

    Create a benchmarking script

---
+++ benchmarks/sql-injection/detectSQLInjection.ts
@@ -0,0 +1,31 @@
+/**
+ * Runs benchmarks for the detection of SQL Injections
+ * @module
+ */
+const { performance } = require('perf_hooks');
+const fs = require('fs');
+import { detectSQLInjection } from "../../library/src/vulnerabilities/detectSQLInjection";
+
+const SQL_STATEMENT = "TEST";
+const USER_INPUT = "T";
+const MAX_TIME = 0.3; // In milliseconds
+
+function runBenchmark() {
+    const startTime = performance.now();
+    const bool = detectSQLInjection(SQL_STATEMENT, USER_INPUT);
+    const endTime = performance.now();
+    return endTime - startTime;
+}
+
+function getAvgBenchmark() {
+    let avgTime = 0;
+    for (let i = 0; i < 25; i++) {
+        console.log(`Run : ${i + 1} - detectSQLInjection()`);
+        avgTime += runBenchmark();
+    }
+    avgTime = avgTime / 10;
+
+    console.log(`Benchmark complete: ${avgTime}ms`)
+}
+
+getAvgBenchmark()
