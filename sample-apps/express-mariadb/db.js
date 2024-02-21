24651ef7a4019d1e728e651f2c574651806d2b79
BitterPanda <gitlab+dev@woutfeys.be>
2024-02-21 11:27:27 +0100

    Change from postgres to mariadb connector

--- sample-apps/express-mariadb/db.js
+++ sample-apps/express-mariadb/db.js
@@ -1,11 +1,11 @@
-const { Client } = require('pg');
-let client;
+const mariadb = require('mariadb');
+let conn;
 
 async function connectToMariaDB() {
     // Normally you'd use environment variables for this
     // These values were also referenced in the docker-compose.yml
     
-    client = new Client({
+    pool = new mariadb.createPool({
         user: 'root',
         host: '127.0.0.1',
         database: 'main_db',
@@ -13,14 +13,14 @@ async function connectToMariaDB() {
         port: 27016,
     });
 
-    await client.connect();
+    conn = await pool.getConnection();
 
     await initDb();
 }
 
 async function initDb() {
     // This creates the cats table
-    await client.query(`
+    await conn.query(`
     CREATE TABLE IF NOT EXISTS cats (
         petname varchar(255)
     );
@@ -29,12 +29,12 @@ async function initDb() {
 
 async function insertCatIntoTable(petname) {
     // This makes your database vulnerable to SQL Injections! vvv
-    await client.query(`INSERT INTO cats(petname) VALUES ('${petname}');`)
+    await conn.query(`INSERT INTO cats(petname) VALUES ('${petname}');`)
 }
 
 async function getAllCats() {
     // This function returns all cats in the db
-    const cats = await client.query("SELECT petname FROM cats;")   
+    const cats = await conn.query("SELECT petname FROM cats;")   
     return cats.rows.map((row) => row.petname);
 }
 
