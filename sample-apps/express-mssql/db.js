0374527ab20cc76005d0f6325e4e98610360c558
BitterPanda <gitlab+dev@woutfeys.be>
2024-02-22 19:23:38 +0100

    Linting + update code with mssql package

--- sample-apps/express-mssql/db.js
+++ sample-apps/express-mssql/db.js
@@ -1,17 +1,18 @@
 // Get the client
-const mssql = require('mssql');
-let connection;
+const sql = require("mssql");
 
 async function connectToMysqlDB() {
-    // Normally you'd use environment variables for this
-    // These values were also referenced in the docker-compose.yml
-    
-    await initDb();
+  // Normally you'd use environment variables for this
+  // These values were also referenced in the docker-compose.yml
+  await sql.connect(
+    "Server=localhost,27014;Database=database;User Id=username;Password=password;Encrypt=true"
+  );
+  await initDb();
 }
 
 async function initDb() {
-    // This creates the cats table
-    await connection.execute(`
+  // This creates the cats table
+  await sql.query(`
     CREATE TABLE IF NOT EXISTS cats (
         petname varchar(255)
     );
@@ -19,14 +20,14 @@ async function initDb() {
 }
 
 async function insertCatIntoTable(petname) {
-    // This makes your database vulnerable to SQL Injections! vvv
-    await connection.query(`INSERT INTO cats(petname) VALUES ('${petname}');`)
+  // This makes your database vulnerable to SQL Injections! vvv
+  await sql.query(`INSERT INTO cats(petname) VALUES ('${petname}');`);
 }
 
 async function getAllCats() {
-    // This function returns all cats in the db
-    const [cats] = await connection.execute("SELECT petname FROM `cats`;")   
-    return cats.map((row) => row.petname);
+  // This function returns all cats in the db
+  const [cats] = await sql.execute("SELECT petname FROM `cats`;");
+  return cats.map((row) => row.petname);
 }
 
-module.exports = {connectToMysqlDB, insertCatIntoTable, getAllCats};
+module.exports = { connectToMysqlDB, insertCatIntoTable, getAllCats };
