a1e4fa96ed2deab8a79f77fa1697d97a6f451606
BitterPanda <gitlab+dev@woutfeys.be>
2024-02-22 14:15:39 +0100

    Add the same code as with postgres

--- library/src/sinks/Mysql2.ts
+++ library/src/sinks/Mysql2.ts
@@ -1,19 +1,43 @@
 import { getInstance } from "../agent/AgentSingleton";
 import { getContext } from "../agent/Context";
 import { WrapSelector, Wrapper } from "../agent/Wrapper";
+import { checkContextForSqlInjection } from "../vulnerabilities/sql-injection/detectSQLInjection";
 
 const EXAMPLE_PACKAGE_VERSION_RANGE = "^3.9.0";
 
 export class Mysql2 extends Wrapper {
   constructor() {
-    const functionWrapSelector: WrapSelector = {
-      exportsSelector: (exports: any) => [exports.myObject.prototype],
+    const queryWrapSelector: WrapSelector = {
+      exportsSelector: (exports: any) => [exports.Connection.prototype],
       middleware: Mysql2.middleware,
     };
 
-    super("pg", EXAMPLE_PACKAGE_VERSION_RANGE, {
-      my_function: functionWrapSelector,
+    super("mysql2", EXAMPLE_PACKAGE_VERSION_RANGE, {
+      createQuery: queryWrapSelector,
     });
   }
-  static middleware(args: unknown[]) {}
+  static middleware(args: unknown[]) {
+    const agent = getInstance();
+    if (!agent) {
+      return;
+    }
+
+    const request = getContext();
+    if (!request) {
+      agent.onInspectedCall({
+        module: "mysql2",
+        withoutContext: true,
+        detectedAttack: false,
+      });
+
+      return;
+    }
+    if (typeof args[0] !== "string") {
+      // The query is not a string, not much to do here
+      return;
+    }
+    const querystring: string = args[0];
+
+    checkContextForSqlInjection(querystring, request, agent, "postgres");
+  }
 }
