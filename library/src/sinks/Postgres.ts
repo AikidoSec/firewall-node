6e33ae777a9a317ea6e411399583973ac2ac4f8c
BitterPanda <gitlab+dev@woutfeys.be>
2024-02-22 11:25:50 +0100

    Upgrade postgres sink to use the new wrapper class

--- library/src/sinks/Postgres.ts
+++ library/src/sinks/Postgres.ts
@@ -1,4 +1,4 @@
-import { Wrapper } from "../agent/Wrapper";
+import { WrapSelector, Wrapper } from "../agent/Wrapper";
 import { Client } from "pg";
 import { Hook } from "require-in-the-middle";
 import { massWrap } from "shimmer";
@@ -6,51 +6,42 @@ import { getInstance } from "../agent/AgentSingleton";
 import { getContext } from "../agent/Context";
 import { checkContextForSqlInjection } from "../vulnerabilities/sql-injection/detectSQLInjection";
 
-export class Postgres implements Wrapper {
-  private wrapQueryFunction(exports: unknown) {
-    const that = this;
-
-    massWrap(
-      // @ts-expect-error This is magic that TypeScript doesn't understand
-      [exports.Client.prototype, exports.Pool.prototype],
-      ["query"],
-      function wrapQueryFunction(original) {
-        return function safeQueryFunction(this: Client, ...args: unknown[]) {
-          const agent = getInstance();
-          if (!agent) {
-            return original.apply(this, args);
-          }
-
-          const request = getContext();
-          if (!request) {
-            agent.onInspectedCall({
-              module: "postgres",
-              withoutContext: true,
-              detectedAttack: false,
-            });
-
-            return original.apply(this, args);
-          }
-          if (typeof args[0] !== "string") {
-            // The query is not a string, not much to do here
-            return original.apply(this, args);
-          }
-          const querystring: string = args[0];
-
-          checkContextForSqlInjection(querystring, request, agent, "postgres");
-
-          return original.apply(this, args);
-        };
-      }
-    );
+export class Postgres extends Wrapper {
+  constructor() {
+    super("pg", postgresWrapSelector, Postgres.middleware);
   }
+  static middleware(args: unknown[]) {
+    const agent = getInstance();
+    if (!agent) {
+      return;
+    }
 
-  private onModuleRequired<T>(exports: T): T {
-    this.wrapQueryFunction(exports);
-    return exports;
-  }
+    const request = getContext();
+    if (!request) {
+      agent.onInspectedCall({
+        module: "postgres",
+        withoutContext: true,
+        detectedAttack: false,
+      });
 
-  wrap() {
-    new Hook(["pg"], this.onModuleRequired.bind(this));
+      return;
+    }
+    if (typeof args[0] !== "string") {
+      // The query is not a string, not much to do here
+      return;
+    }
+    const querystring: string = args[0];
+
+    checkContextForSqlInjection(querystring, request, agent, "postgres");
   }
+}
+
+const postgresWrapSelector: WrapSelector = {
+  wrapFunctions: ["query"],
+  exportsSelector,
+};
+
+function exportsSelector(exports: unknown) {
+  // @ts-expect-error This is magic that TypeScript doesn't understand
+  return [exports.Client.prototype, exports.Pool.prototype];
 }
