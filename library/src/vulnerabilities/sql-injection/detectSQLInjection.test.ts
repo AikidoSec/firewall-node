7017ae8eb60da7c72d06aa86b5b7800d6ba13e0c
BitterPanda <gitlab+dev@woutfeys.be>
2024-02-22 10:35:42 +0100

    Add new tests to allow single characters through

--- library/src/vulnerabilities/sql-injection/detectSQLInjection.test.ts
+++ library/src/vulnerabilities/sql-injection/detectSQLInjection.test.ts
@@ -47,12 +47,16 @@ const GOOD_SQL_COMMANDS = [
   "I was benchmark ing",
   "We were delay ed",
   "I will waitfor you",
+  // Allow single characters
+  "#",
+  "'",
 ];
 
 const IS_NOT_INJECTION = [
   [`'UNION 123' UNION "UNION 123"`, "UNION 123"], // String encapsulation
   [`'union'  is not "UNION"`, "UNION!"], // String not present in SQL
   [`"UNION;"`, "UNION;"], // String encapsulation
+  ["SELECT * FROM table", "*"],
 ];
 
 const IS_INJECTION = [
@@ -202,10 +206,10 @@ t.test("Test the dangerousCharsInInput() function", async () => {
   t.ok(dangerousCharsInInput("This is not ok--"));
 });
 
-function t_isSqlInjection(sql, input) {
+function t_isSqlInjection(sql: string, input: string) {
   t.ok(detectSQLInjection(sql, input), sql);
 }
 
-function t_isNotSqlInjection(sql, input) {
+function t_isNotSqlInjection(sql: string, input: string) {
   t.notOk(detectSQLInjection(sql, input), sql);
 }
