669ceb22961bb3f49fca9cbd193983072db07883
BitterPanda <gitlab+dev@woutfeys.be>
2024-02-23 09:11:53 +0100

    Import mssql in protect.ts

--- library/src/agent/protect.ts
+++ library/src/agent/protect.ts
@@ -8,13 +8,14 @@ import { Express } from "../sources/Express";
 import { createLambdaWrapper } from "../sources/Lambda";
 import { MongoDB } from "../sinks/MongoDB";
 import { Postgres } from "../sinks/Postgres";
+import { MSSQL } from "../sinks/MSSQL";
 import * as shimmer from "shimmer";
 import { Logger, LoggerConsole, LoggerNoop } from "./Logger";
 import { Wrapper } from "./Wrapper";
 import { Options, getOptions } from "../helpers/getOptions";
 
 function wrapInstalledPackages() {
-  const packages = [new Postgres(), new MongoDB(), new Express()];
+  const packages = [new Postgres(), new MongoDB(), new Express(), new MSSQL()];
 
   const wrapped: Record<string, { version: string; supported: boolean }> = {};
   for (const wrapper of packages) {
