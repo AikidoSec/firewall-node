e3d9066434fb07f7bb802149daf13c0393c37461
BitterPanda <gitlab+dev@woutfeys.be>
2024-02-23 10:45:17 +0100

    Install next  + add start of basic middleware

--- library/src/sources/Next.ts
+++ library/src/sources/Next.ts
@@ -1,3 +1,10 @@
-export function nextMiddleware() {
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
+export function nextMiddleware(request:NextRequest) {
+    const url = request.nextUrl;
+    const cookies = request.cookies.getAll();
+
+    const response = NextResponse.next();
+    return response;
 }
