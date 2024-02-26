415d96c2d17d17b1b1abcc7c4bb7a693e27a59db
Hans Ott <hansott@hotmail.be>
2024-02-26 17:24:50 +0100

    Use unknown instead of any

--- library/src/helpers/extractStringsFromObjects.ts
+++ library/src/helpers/extractStringsFromObjects.ts
@@ -2,21 +2,25 @@ import { isPlainObject } from "./isPlainObject";
 
 /**
  * This checks the object for user input
- * @param obj The object you want to analyze for user input
- * @returns User input found in the obj as an array of strings
  */
-export function extract(obj: any): string[] {
+export function extract(obj: unknown): string[] {
   let results: Set<string> = new Set();
+
   if (isPlainObject(obj)) {
     for (const key in obj) {
       results = new Set([key, ...results, ...extract(obj[key])]);
     }
-  } else if (Array.isArray(obj)) {
+  }
+
+  if (Array.isArray(obj)) {
     for (const element of obj) {
       results = new Set([...results, ...extract(element)]);
     }
-  } else if (typeof obj == "string") {
+  }
+
+  if (typeof obj == "string") {
     results.add(obj);
   }
+
   return Array.from(results);
 }
