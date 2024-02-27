d33100eb3647f552c994c20cdbf8afe5b2dbdb78
Hans Ott <hansott@hotmail.be>
2024-02-27 18:12:08 +0100

    Add tests for successful JWT decode

--- library/src/helpers/tryDecodeAsJWT.test.ts
+++ library/src/helpers/tryDecodeAsJWT.test.ts
@@ -11,3 +11,42 @@ t.test("it returns undefined for invalid JWT", async () => {
   t.same(tryDecodeAsJWT("invalid.invalid.invalid"), { jwt: false });
   t.same(tryDecodeAsJWT("invalid.invalid.invalid.invalid"), { jwt: false });
 });
+
+t.test("it returns the decoded JWT for valid JWT", async () => {
+  t.same(
+    tryDecodeAsJWT(
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidXNlcm5hbWUiOnsiJG5lIjpudWxsfSwiaWF0IjoxNTE2MjM5MDIyfQ._jhGJw9WzB6gHKPSozTFHDo9NOHs3CNOlvJ8rWy6VrQ"
+    ),
+    {
+      jwt: true,
+      object: {
+        sub: "1234567890",
+        username: {
+          $ne: null,
+        },
+        iat: 1516239022,
+      },
+    }
+  );
+});
+
+t.test(
+  "it returns the decoded JWT for valid JWT with bearer prefix",
+  async () => {
+    t.same(
+      tryDecodeAsJWT(
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidXNlcm5hbWUiOnsiJG5lIjpudWxsfSwiaWF0IjoxNTE2MjM5MDIyfQ._jhGJw9WzB6gHKPSozTFHDo9NOHs3CNOlvJ8rWy6VrQ"
+      ),
+      {
+        jwt: true,
+        object: {
+          sub: "1234567890",
+          username: {
+            $ne: null,
+          },
+          iat: 1516239022,
+        },
+      }
+    );
+  }
+);
