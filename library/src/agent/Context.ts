6ad28aad13e89ab2ead94c597fc75e18f3db9105
Hans Ott <hansott@hotmail.be>
2024-02-27 11:59:55 +0100

    Improve comments

--- library/src/agent/Context.ts
+++ library/src/agent/Context.ts
@@ -13,10 +13,20 @@ export type Context = {
 
 const requestContext = new AsyncLocalStorage<Context>();
 
+/**
+ * Get the current request context that is being handled
+ */
 export function getContext() {
   return requestContext.getStore();
 }
 
+/**
+ * Executes a function with a given request context
+ *
+ * The code executed inside the function will have access to the context using {@link getContext}
+ *
+ * This is needed because Node.js is single-threaded, so we can't use a global variable to store the context.
+ */
 export function runWithContext<T>(context: Context, fn: () => T) {
   return requestContext.run(context, fn);
 }
