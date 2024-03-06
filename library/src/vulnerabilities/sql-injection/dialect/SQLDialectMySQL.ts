80f9670d04a286a761f767ee9f7cfd794ad1c6f7
Hans Ott <hansott@hotmail.be>
2024-03-06 13:23:00 +0100

    Simplify

--- library/src/vulnerabilities/sql-injection/dialect/SQLDialectMySQL.ts
+++ library/src/vulnerabilities/sql-injection/dialect/SQLDialectMySQL.ts
@@ -4,38 +4,26 @@ export class SQLDialectMySQL implements SQLDialect {
   getEscapedRanges(sql: string): Range[] {
     const ranges: Range[] = [];
     let literal: { start: number; quote: string } | undefined = undefined;
+    const escapeQuotes = ["'", '"'];
 
     for (let i = 0; i < sql.length; i++) {
       const char = sql[i];
 
-      if (char === "'") {
-        if (literal && literal.quote === "'") {
-          if (sql[i + 1] === "'") {
-            i++;
+      for (const quote of escapeQuotes) {
+        if (char === quote) {
+          if (literal && literal.quote === quote) {
+            if (sql[i + 1] === quote) {
+              i++;
+              continue;
+            }
+
+            ranges.push([literal.start, i, sql.slice(literal.start + 1, i)]);
+            literal = undefined;
             continue;
           }
 
-          ranges.push([literal.start, i, sql.slice(literal.start + 1, i)]);
-          literal = undefined;
-          continue;
+          literal = { start: i, quote: char };
         }
-
-        literal = { start: i, quote: char };
-      }
-
-      if (char === '"') {
-        if (literal && literal.quote === '"') {
-          if (sql[i + 1] === '"') {
-            i++;
-            continue;
-          }
-
-          ranges.push([literal.start, i, sql.slice(literal.start + 1, i)]);
-          literal = undefined;
-          continue;
-        }
-
-        literal = { start: i, quote: char };
       }
     }
 
