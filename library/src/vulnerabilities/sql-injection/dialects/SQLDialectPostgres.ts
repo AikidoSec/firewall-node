2799fadbd49c38ffedaab75ace61d014eaeaec28
Hans Ott <hansott@hotmail.be>
2024-03-13 15:21:09 +0100

    Add comment

--- library/src/vulnerabilities/sql-injection/dialects/SQLDialectPostgres.ts
+++ library/src/vulnerabilities/sql-injection/dialects/SQLDialectPostgres.ts
@@ -3,6 +3,7 @@ import { SQLDialect } from "./SQLDialect";
 
 export class SQLDialectPostgres implements SQLDialect {
   getDangerousStrings(): string[] {
+    // https://www.postgresql.org/docs/current/sql-syntax-lexical.html#SQL-SYNTAX-DOLLAR-QUOTING
     return ["$"];
   }
 }
