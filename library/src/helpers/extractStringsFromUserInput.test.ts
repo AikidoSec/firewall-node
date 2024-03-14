16e9519a436667603a15cd433d4c73350f206dfd
Hans Ott <hansott@hotmail.be>
2024-03-14 10:56:08 +0100

    Calculate path to SQL injection payload
    AIK-2529 #close

--- library/src/helpers/extractStringsFromUserInput.test.ts
+++ library/src/helpers/extractStringsFromUserInput.test.ts
@@ -7,37 +7,42 @@ t.test("empty object returns empty array", async () => {
 });
 
 t.test("it can extract query objects", async () => {
-  t.same(extractStringsFromUserInput({ age: { $gt: "21" } }), [
-    "age",
-    "$gt",
-    "21",
-  ]);
-  t.same(extractStringsFromUserInput({ title: { $ne: "null" } }), [
-    "title",
-    "$ne",
-    "null",
-  ]);
+  t.same(extractStringsFromUserInput({ age: { $gt: "21" } }), {
+    age: ".",
+    $gt: ".age",
+    "21": ".age.$gt",
+  });
+  t.same(extractStringsFromUserInput({ title: { $ne: "null" } }), {
+    title: ".",
+    $ne: ".title",
+    null: ".title.$ne",
+  });
   t.same(
     extractStringsFromUserInput({
       age: "whaat",
       user_input: ["whaat", "dangerous"],
     }),
-    ["user_input", "age", "whaat", "dangerous"]
+    {
+      user_input: ".",
+      age: ".",
+      whaat: ".user_input.[0]",
+      dangerous: ".user_input.[1]",
+    }
   );
 });
 
 t.test("it can extract cookie objects", async () => {
-  t.same(extractStringsFromUserInput({ session: "ABC", session2: "DEF" }), [
-    "session2",
-    "session",
-    "ABC",
-    "DEF",
-  ]);
-  t.same(extractStringsFromUserInput({ session: "ABC", session2: 1234 }), [
-    "session2",
-    "session",
-    "ABC",
-  ]);
+  t.same(extractStringsFromUserInput({ session: "ABC", session2: "DEF" }), {
+    session2: ".",
+    session: ".",
+    ABC: ".session",
+    DEF: ".session2",
+  });
+  t.same(extractStringsFromUserInput({ session: "ABC", session2: 1234 }), {
+    session2: ".",
+    session: ".",
+    ABC: ".session",
+  });
 });
 
 t.test("it can extract header objects", async () => {
@@ -45,13 +50,18 @@ t.test("it can extract header objects", async () => {
     extractStringsFromUserInput({
       "Content-Type": "application/json",
     }),
-    ["Content-Type", "application/json"]
+    {
+      "Content-Type": ".",
+      "application/json": ".Content-Type",
+    }
   );
   t.same(
     extractStringsFromUserInput({
       "Content-Type": 54321,
     }),
-    ["Content-Type"]
+    {
+      "Content-Type": ".",
+    }
   );
   t.notSame(
     extractStringsFromUserInput({
@@ -63,26 +73,42 @@ t.test("it can extract header objects", async () => {
 });
 
 t.test("it can extract body objects", async () => {
-  t.same(extractStringsFromUserInput({ nested: { nested: { $ne: null } } }), [
-    "nested",
-    "$ne",
-  ]);
+  t.same(extractStringsFromUserInput({ nested: { nested: { $ne: null } } }), {
+    nested: ".nested",
+    $ne: ".nested.nested",
+  });
 
-  t.same(extractStringsFromUserInput({ age: { $gt: "21", $lt: "100" } }), [
-    "age",
-    "$lt",
-    "$gt",
-    "21",
-    "100",
-  ]);
+  t.same(extractStringsFromUserInput({ age: { $gt: "21", $lt: "100" } }), {
+    age: ".",
+    $lt: ".age",
+    $gt: ".age",
+    "21": ".age.$gt",
+    "100": ".age.$lt",
+  });
 });
 
 t.test("it decodes JWTs", async () => {
   t.same(
     extractStringsFromUserInput({
+      /**
+       * {
+       *   sub: "1234567890",
+       *   username: {
+       *     $ne: null,
+       *   },
+       *   iat: 1516239022,
+       * }
+       */
       token:
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidXNlcm5hbWUiOnsiJG5lIjpudWxsfSwiaWF0IjoxNTE2MjM5MDIyfQ._jhGJw9WzB6gHKPSozTFHDo9NOHs3CNOlvJ8rWy6VrQ",
     }),
-    ["token", "iat", "username", "sub", "1234567890", "$ne"]
+    {
+      token: ".",
+      iat: ".token<jwt>",
+      username: ".token<jwt>",
+      sub: ".token<jwt>",
+      "1234567890": ".token<jwt>.sub",
+      $ne: ".token<jwt>.username",
+    }
   );
 });
--- library/src/helpers/extractStringsFromUserInput.ts
+++ library/src/helpers/extractStringsFromUserInput.ts
@@ -1,39 +1,58 @@
+import { buildPathToPayload, PathPart } from "./attackPath";
 import { isPlainObject } from "./isPlainObject";
 import { tryDecodeAsJWT } from "./tryDecodeAsJWT";
+
+type UserString = string;
+type PathToUserString = string;
 
 /**
  * Extract all strings from an object, see unit tests for examples
  */
-export function extractStringsFromUserInput(obj: unknown): string[] {
-  let results: Set<string> = new Set();
+export function extractStringsFromUserInput(
+  obj: unknown,
+  pathToPayload: PathPart[] = []
+): Record<UserString, PathToUserString> {
+  const results: Record<UserString, PathToUserString> = {};
 
   if (isPlainObject(obj)) {
     for (const key in obj) {
-      results = new Set([
-        key,
-        ...results,
-        ...extractStringsFromUserInput(obj[key]),
-      ]);
+      results[key] = buildPathToPayload(pathToPayload);
+      const strings = extractStringsFromUserInput(
+        obj[key],
+        pathToPayload.concat([{ type: "object", key: key }])
+      );
+      for (const key in strings) {
+        results[key] = strings[key];
+      }
     }
   }
 
   if (Array.isArray(obj)) {
-    for (const element of obj) {
-      results = new Set([...results, ...extractStringsFromUserInput(element)]);
+    for (let i = 0; i < obj.length; i++) {
+      const strings = extractStringsFromUserInput(
+        obj[i],
+        pathToPayload.concat([{ type: "array", index: i }])
+      );
+      for (const nestedKey in strings) {
+        results[nestedKey] = strings[nestedKey];
+      }
     }
   }
 
   if (typeof obj == "string") {
     const jwt = tryDecodeAsJWT(obj);
     if (jwt.jwt) {
-      results = new Set([
-        ...results,
-        ...extractStringsFromUserInput(jwt.object),
-      ]);
+      const nestedResults = extractStringsFromUserInput(
+        jwt.object,
+        pathToPayload.concat([{ type: "jwt" }])
+      );
+      for (const nestedKey in nestedResults) {
+        results[nestedKey] = nestedResults[nestedKey];
+      }
     } else {
-      results.add(obj);
+      results[obj] = buildPathToPayload(pathToPayload);
     }
   }
 
-  return Array.from(results);
+  return results;
 }
