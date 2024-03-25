94bf50a4174fcaf4d5ba26e5f43000341a7bad77
Hans Ott <hansott@hotmail.be>
2024-03-25 12:42:03 +0100

    Add tests for new line

--- library/src/vulnerabilities/shell-injection/containsShellSyntax.test.ts
+++ library/src/vulnerabilities/shell-injection/containsShellSyntax.test.ts
@@ -4,7 +4,14 @@ import { containsShellSyntax } from "./containsShellSyntax";
 t.test("it detects shell syntax", async (t) => {
   t.same(containsShellSyntax(""), false);
   t.same(containsShellSyntax("hello"), false);
+  t.same(containsShellSyntax("\n"), false);
+  t.same(containsShellSyntax("\n\n"), false);
+
   t.same(containsShellSyntax("$(command)"), true);
   t.same(containsShellSyntax("$(command arg arg)"), true);
   t.same(containsShellSyntax("`command`"), true);
+  t.same(containsShellSyntax("\narg"), true);
+  t.same(containsShellSyntax("\narg\n"), true);
+  t.same(containsShellSyntax("arg\n"), true);
+  t.same(containsShellSyntax("arg\narg"), true);
 });
