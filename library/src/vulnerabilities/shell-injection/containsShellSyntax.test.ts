bbec72521d6167a6d22012091f2d2a567f7f6bed
Hans Ott <hansott@hotmail.be>
2024-03-25 18:47:49 +0100

    Extract function

--- library/src/vulnerabilities/shell-injection/containsShellSyntax.test.ts
+++ library/src/vulnerabilities/shell-injection/containsShellSyntax.test.ts
@@ -2,17 +2,21 @@ import * as t from "tap";
 import { containsShellSyntax } from "./containsShellSyntax";
 
 t.test("it detects shell syntax", async (t) => {
-  t.same(containsShellSyntax(""), false);
-  t.same(containsShellSyntax("hello"), false);
-  t.same(containsShellSyntax("\n"), false);
-  t.same(containsShellSyntax("\n\n"), false);
+  containsSyntax("", false);
+  containsSyntax("hello", false);
+  containsSyntax("\n", false);
+  containsSyntax("\n\n", false);
 
-  t.same(containsShellSyntax("$(command)"), true);
-  t.same(containsShellSyntax("$(command arg arg)"), true);
-  t.same(containsShellSyntax("`command`"), true);
-  t.same(containsShellSyntax("\narg"), true);
-  t.same(containsShellSyntax("\narg\n"), true);
-  t.same(containsShellSyntax("arg\n"), true);
-  t.same(containsShellSyntax("arg\narg"), true);
-  t.same(containsShellSyntax("rm -rf"), true);
+  containsSyntax("$(command)", true);
+  containsSyntax("$(command arg arg)", true);
+  containsSyntax("`command`", true);
+  containsSyntax("\narg", true);
+  containsSyntax("\narg\n", true);
+  containsSyntax("arg\n", true);
+  containsSyntax("arg\narg", true);
+  containsSyntax("rm -rf", true);
 });
+
+function containsSyntax(str: string, expected: boolean) {
+  t.same(containsShellSyntax(str), expected);
+}
