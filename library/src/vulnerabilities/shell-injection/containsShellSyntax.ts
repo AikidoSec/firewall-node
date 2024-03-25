875d0af2ab75aac3e42197a109506a92026d08e1
Hans Ott <hansott@hotmail.be>
2024-03-25 14:31:35 +0100

    Add comment for regex

--- library/src/vulnerabilities/shell-injection/containsShellSyntax.ts
+++ library/src/vulnerabilities/shell-injection/containsShellSyntax.ts
@@ -26,6 +26,10 @@ const dangerousShellStrings = [
   "}",
 ];
 
+// Just whitespace characters alone are not dangerous
+// They can occur in user input without posing a threat.
+// So a space might occur in the command and in the user input (being an exact match)
+// But when a space is not standalone, it is dangerous, because the user input might start a new command or argument
 const regexForNonStandaloneSpaces = /(\b[ \n])|(\b[ \n]\b)|([ \n]\b)/;
 
 export function containsShellSyntax(userInput: string): boolean {
