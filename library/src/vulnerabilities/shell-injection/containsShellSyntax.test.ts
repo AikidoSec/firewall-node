0f591625e8d6561cb1284dc15106c32c221e177c
Hans Ott <hansott@hotmail.be>
2024-03-26 10:05:41 +0100

    Reset lastIndex on regex object
    Because we're using the `g` flag, the regex keeps track of `exec(...)`
    calls. We need to reset it so that it starts from the beginning.
    
    for (const match of matchAll(command, commandsRegex)) {
        // If the command doesn't contain the user input, we can skip this match
        if (userInput !== match[0]) {
          continue;
        }
    
    Also, we need to use the actual match ^ here to check if that actual
    command occurs is the userInput. Not just execute a regex again because
    it might match another command from the regex.

--- library/src/vulnerabilities/shell-injection/containsShellSyntax.test.ts
+++ library/src/vulnerabilities/shell-injection/containsShellSyntax.test.ts
@@ -16,8 +16,39 @@ t.test("it detects shell syntax", async (t) => {
   containsSyntax("arg\n", true);
   containsSyntax("arg\narg", true);
   containsSyntax("rm -rf", true);
+  containsSyntax("/bin/rm -rf", true);
+  containsSyntax("/bin/rm", true);
+  containsSyntax("/sbin/sleep", true);
 });
 
 function containsSyntax(str: string, expected: boolean) {
   t.same(containsShellSyntax(str, str), expected, str);
 }
+
+t.test("it detects commands surrounded by separators", async (t) => {
+  t.same(
+    containsShellSyntax(
+      `find /path/to/search -type f -name "pattern" -exec rm {} \\\\;`,
+      "rm"
+    ),
+    true
+  );
+});
+
+t.test("it detects commands with separator before", async (t) => {
+  t.same(
+    containsShellSyntax(
+      `find /path/to/search -type f -name "pattern" | xargs rm`,
+      "rm"
+    ),
+    true
+  );
+});
+
+t.test("it detects commands with separator after", async (t) => {
+  t.same(containsShellSyntax("rm arg", "rm"), true);
+});
+
+t.test("it checks if the same command occurs in the user input", async () => {
+  t.same(containsShellSyntax("find cp", "rm"), false);
+});
--- library/src/vulnerabilities/shell-injection/containsShellSyntax.ts
+++ library/src/vulnerabilities/shell-injection/containsShellSyntax.ts
@@ -82,6 +82,9 @@ const commandsRegex = new RegExp(
 );
 
 function matchAll(str: string, regex: RegExp) {
+  // Reset the regex so that we can use it multiple times
+  regex.lastIndex = 0;
+
   const matches = [];
   let match;
   while ((match = regex.exec(str)) !== null) {
@@ -105,26 +108,42 @@ export function containsShellSyntax(
     return true;
   }
 
-  if (command === userInput && commands.find((c) => c === userInput)) {
-    return true;
+  if (command === userInput) {
+    // Reset the regex so that we can use it multiple times
+    commandsRegex.lastIndex = 0;
+    const match = commandsRegex.exec(command);
+
+    return match
+      ? match.index === 0 && match[0].length === command.length
+      : false;
   }
 
+  // Check if the command contains a dangerous command
   for (const match of matchAll(command, commandsRegex)) {
-    if (!commandsRegex.test(userInput)) {
+    // If the command doesn't contain the user input, we can skip this match
+    if (userInput !== match[0]) {
       continue;
     }
 
+    // Check if the command is surrounded by separators
+    // These separators are used to separate commands and arguments
+    // e.g. `rm<space>-rf`
+    // e.g. `ls<newline>whoami`
+    // e.g. `echo<tab>hello`
     const charBefore = command[match.index - 1];
     const charAfter = command[match.index + match[0].length];
 
+    // e.g. `<separator>rm<separator>`
     if (separators.includes(charBefore) && separators.includes(charAfter)) {
       return true;
     }
 
+    // e.g. `<separator>rm`
     if (separators.includes(charBefore) && charAfter === undefined) {
       return true;
     }
 
+    // e.g. `rm<separator>`
     if (charBefore === undefined && separators.includes(charAfter)) {
       return true;
     }
