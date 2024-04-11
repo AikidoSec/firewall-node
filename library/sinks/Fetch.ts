da5367dda53fc9cea17c1a2d05daf7d22a17c515
Hans Ott <hansott@hotmail.be>
2024-04-11 13:25:34 +0200

    Use private method to see every hostname we connect to

--- library/sinks/Fetch.ts
+++ library/sinks/Fetch.ts
@@ -1,16 +1,34 @@
 import { Agent } from "../agent/Agent";
 import { Hooks } from "../agent/hooks/Hooks";
+import { InterceptorResult } from "../agent/hooks/MethodInterceptor";
 import { Wrapper } from "../agent/Wrapper";
 import { getPortFromURL } from "../helpers/getPortFromURL";
 
 export class Fetch implements Wrapper {
-  inspectFetch(args: unknown[], agent: Agent) {
+  private onConnectHostname(
+    agent: Agent,
+    hostname: string,
+    port: number | undefined
+  ): InterceptorResult {
+    agent.onConnectHostname(hostname, port);
+
+    return undefined;
+  }
+
+  inspectFetch(args: unknown[], agent: Agent): InterceptorResult {
     if (args.length > 0) {
       if (typeof args[0] === "string" && args[0].length > 0) {
         try {
           const url = new URL(args[0]);
           if (url.hostname.length > 0) {
-            agent.onConnectHostname(url.hostname, getPortFromURL(url));
+            const result = this.onConnectHostname(
+              agent,
+              url.hostname,
+              getPortFromURL(url)
+            );
+            if (result) {
+              return result;
+            }
           }
         } catch (e) {
           // Ignore
@@ -18,7 +36,14 @@ export class Fetch implements Wrapper {
       }
 
       if (args[0] instanceof URL && args[0].hostname.length > 0) {
-        agent.onConnectHostname(args[0].hostname, getPortFromURL(args[0]));
+        const result = this.onConnectHostname(
+          agent,
+          args[0].hostname,
+          getPortFromURL(args[0])
+        );
+        if (result) {
+          return result;
+        }
       }
     }
   }
--- library/sinks/HTTPRequest.ts
+++ library/sinks/HTTPRequest.ts
@@ -1,17 +1,35 @@
 import { Agent } from "../agent/Agent";
 import { Hooks } from "../agent/hooks/Hooks";
+import { InterceptorResult } from "../agent/hooks/MethodInterceptor";
 import { Wrapper } from "../agent/Wrapper";
 import { getPortFromURL } from "../helpers/getPortFromURL";
 import { isPlainObject } from "../helpers/isPlainObject";
 
 export class HTTPRequest implements Wrapper {
+  private onConnectHostname(
+    agent: Agent,
+    hostname: string,
+    port: number | undefined
+  ): InterceptorResult {
+    agent.onConnectHostname(hostname, port);
+
+    return undefined;
+  }
+
   inspectHttpRequest(args: unknown[], agent: Agent) {
     if (args.length > 0) {
       if (typeof args[0] === "string" && args[0].length > 0) {
         try {
           const url = new URL(args[0]);
           if (url.hostname.length > 0) {
-            agent.onConnectHostname(url.hostname, getPortFromURL(url));
+            const result = this.onConnectHostname(
+              agent,
+              url.hostname,
+              getPortFromURL(url)
+            );
+            if (result) {
+              return result;
+            }
           }
         } catch (e) {
           // Ignore
@@ -19,7 +37,14 @@ export class HTTPRequest implements Wrapper {
       }
 
       if (args[0] instanceof URL && args[0].hostname.length > 0) {
-        agent.onConnectHostname(args[0].hostname, getPortFromURL(args[0]));
+        const result = this.onConnectHostname(
+          agent,
+          args[0].hostname,
+          getPortFromURL(args[0])
+        );
+        if (result) {
+          return result;
+        }
       }
 
       if (
@@ -27,10 +52,14 @@ export class HTTPRequest implements Wrapper {
         typeof args[0].hostname === "string" &&
         args[0].hostname.length > 0
       ) {
-        agent.onConnectHostname(
+        const result = this.onConnectHostname(
+          agent,
           args[0].hostname,
           typeof args[0].port === "number" ? args[0].port : undefined
         );
+        if (result) {
+          return result;
+        }
       }
     }
   }
