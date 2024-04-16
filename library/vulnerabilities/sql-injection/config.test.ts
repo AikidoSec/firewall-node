f800e42f1cccb8931569f2b097ca0abff3ba6ec6
Hans Ott <hansott@hotmail.be>
2024-04-16 18:16:38 +0200

    Make tests more clear

--- library/vulnerabilities/sql-injection/config.test.ts
+++ library/vulnerabilities/sql-injection/config.test.ts
@@ -8,45 +8,49 @@ import {
   SQL_STRING_CHARS,
 } from "./config";
 
-t.test("SQL_KEYWORDS are valid", async () => {
+t.test("SQL_KEYWORDS are not empty", async () => {
   SQL_KEYWORDS.forEach((keyword) => {
     t.ok(keyword.length > 0);
   });
+});
 
+t.test("SQL_KEYWORDS are uppercase", async () => {
   SQL_KEYWORDS.forEach((keyword) => {
     t.same(keyword, keyword.toUpperCase());
   });
 });
 
-t.test("COMMON_SQL_KEYWORDS are valid", async () => {
+t.test("COMMON_SQL_KEYWORDS are not empty", async () => {
   COMMON_SQL_KEYWORDS.forEach((keyword) => {
     t.ok(keyword.length > 0);
   });
+});
 
+t.test("COMMON_SQL_KEYWORDS are uppercase", async () => {
   COMMON_SQL_KEYWORDS.forEach((keyword) => {
     t.same(keyword, keyword.toUpperCase());
   });
 });
 
-t.test("SQL_OPERATORS are valid", async () => {
+t.test("SQL_OPERATORS are not empty", async () => {
   SQL_OPERATORS.forEach((operator) => {
     t.ok(operator.length > 0);
   });
 });
 
-t.test("SQL_STRING_CHARS are valid", async () => {
+t.test("SQL_STRING_CHARS are single chars", async () => {
   SQL_STRING_CHARS.forEach((char) => {
     t.ok(char.length === 1);
   });
 });
 
-t.test("SQL_DANGEROUS_IN_STRING are valid", async () => {
+t.test("SQL_DANGEROUS_IN_STRING are not empty", async () => {
   SQL_DANGEROUS_IN_STRING.forEach((char) => {
     t.ok(char.length > 0);
   });
 });
 
-t.test("SQL_ESCAPE_SEQUENCES are valid", async () => {
+t.test("SQL_ESCAPE_SEQUENCES are not empty", async () => {
   SQL_ESCAPE_SEQUENCES.forEach((sequence) => {
     t.ok(sequence.length > 0);
   });
