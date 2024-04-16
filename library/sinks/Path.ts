6572ba1e6f1935fc7c6549dd6cfbd8d60597fc90
Hans Ott <hansott@hotmail.be>
2024-04-16 14:23:35 +0200

    Check arguments of path.join/resolve for path traversal attacks

---
+++ library/sinks/Path.ts
@@ -0,0 +1,38 @@
+import { getContext } from "../agent/Context";
+import { Hooks } from "../agent/hooks/Hooks";
+import { Wrapper } from "../agent/Wrapper";
+import { checkContextForPathTraversal } from "../vulnerabilities/path-traversal/checkContextForPathTraversal";
+
+export class Path implements Wrapper {
+  private inspectPath(args: unknown[], operation: string) {
+    const context = getContext();
+
+    if (!context) {
+      return undefined;
+    }
+
+    for (const path of args) {
+      if (typeof path === "string") {
+        const result = checkContextForPathTraversal({
+          filename: path,
+          operation: `path.${operation}`,
+          context: context,
+        });
+
+        if (result) {
+          return result;
+        }
+      }
+    }
+
+    return undefined;
+  }
+
+  wrap(hooks: Hooks): void {
+    hooks
+      .addBuiltinModule("path")
+      .addSubject((exports) => exports)
+      .inspect("join", (args) => this.inspectPath(args, "join"))
+      .inspect("resolve", (args) => this.inspectPath(args, "resolve"));
+  }
+}
