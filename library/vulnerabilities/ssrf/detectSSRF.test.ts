bbb1166924bb2bfbe0582a47eb9a7bb4b1ba8426
nadiraikido <166383531+nadiraikido@users.noreply.github.com>
2024-04-16 12:13:04 +0200

    Add more tests

--- library/vulnerabilities/ssrf/detectSSRF.test.ts
+++ library/vulnerabilities/ssrf/detectSSRF.test.ts
@@ -28,6 +28,44 @@ t.test(
   }
 );
 
+t.test("it parses hostname from user input without protocol seperator",
+  async (t) => {
+    t.same(detectSSRF("http:localhost", "localhost"), true);
+  }
+);
+
+t.test("it detects private IP addresses behind a DNS record",
+  async (t) => {
+    t.same(detectSSRF("http://localtest.me", "localtest.me"), true)
+  }
+);
+
+t.test("it detects private IPv4 address using decimal encoding",
+  async (t) => {
+    t.same(detectSSRF("http://2130706433", "2130706433"), true);
+  }
+);
+
+t.test("it detects private IPv4 address using octal encoding",
+    async (t) => {
+      t.same(detectSSRF("http://0177.0.0.1", "0177.0.0.1"), true);
+    }
+);
+
+t.test("it detects private IPv6/IPv4 address embedding",
+    async (t) => {
+      t.same(detectSSRF("http://[0:0:0:0:0:ffff:127.0.0.1]", "[0:0:0:0:0:ffff:127.0.0.1]"), true);
+    }
+);
+
+t.test("it detects private IPv4 address by dropping zeros",
+    async (t) => {
+      t.same(detectSSRF("http://0/", "0"), true);
+      t.same(detectSSRF("http://127.1", "127.1"), true);
+      t.same(detectSSRF("http://127.0.1", "127.0.1"), true);
+    }
+);
+
 t.test(
   "it parses hostname from user input with misspelled protocol and path behind it",
   async (t) => {
@@ -80,6 +118,8 @@ t.test("it checks for private IPv6 address", async (t) => {
   );
   t.same(detectSSRF("http://[fe80::1]", "[fe80::1]"), true);
   t.same(detectSSRF("http://[fd00:ec2::254]", "[fd00:ec2::254]"), true);
+  t.same(detectSSRF("http://[::]:80/", "[::]"), true);
+  t.same(detectSSRF("http://[0000::1]:80/", "[0000::1]"), true);
 });
 
 t.test("it ignores public IPv6 address", async (t) => {
