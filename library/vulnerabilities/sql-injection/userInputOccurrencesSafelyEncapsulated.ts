e3f166bfbcbeae6d3702bb3482df8c060aeae6c1
Hans Ott <hansott@hotmail.be>
2024-04-24 11:03:29 +0200

    Add special case for when user input starts with single quote

--- library/vulnerabilities/sql-injection/userInputOccurrencesSafelyEncapsulated.ts
+++ library/vulnerabilities/sql-injection/userInputOccurrencesSafelyEncapsulated.ts
@@ -14,10 +14,26 @@ export function userInputOccurrencesSafelyEncapsulated(
   const segmentsInBetween = getCurrentAndNextSegments(query.split(userInput));
 
   return segmentsInBetween.every(({ currentSegment, nextSegment }) => {
-    const charBeforeUserInput = currentSegment.slice(-1);
-    const quoteChar = SQL_STRING_CHARS.find(
+    let charBeforeUserInput = currentSegment.slice(-1);
+    let quoteChar = SQL_STRING_CHARS.find(
       (char) => char === charBeforeUserInput
     );
+
+    // Special case for when the user input starts with a single quote
+    // If the user input is `'value`
+    // And the single quote is properly escaped with a backslash we split the following
+    // `SELECT * FROM table WHERE column = '\'value'`
+    // Into [`SELECT * FROM table WHERE column = '\`, `'`]
+    // The char before the user input will be `\` and the char after the user input will be `'`
+    if (
+      !quoteChar &&
+      userInput.startsWith("'") &&
+      currentSegment.slice(-2) === "'\\"
+    ) {
+      quoteChar = "'";
+      charBeforeUserInput = currentSegment.slice(-2, -1);
+      userInput = userInput.slice(1);
+    }
 
     if (!quoteChar) {
       return false;
