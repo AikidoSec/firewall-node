d5a56f4562a14c35962707580b915ddf193f4a02
Hans Ott <hansott@hotmail.be>
2024-05-09 13:09:45 +0200

    Add comment to ConfigAPI

--- library/agent/config-api/ConfigAPI.ts
+++ library/agent/config-api/ConfigAPI.ts
@@ -1,5 +1,10 @@
 import { Token } from "../api/Token";
 
+/**
+ * This API is used to get the last updated timestamp of the configuration.
+ *
+ * The actual configuration can be fetched using the ReportingAPI.
+ */
 export interface ConfigAPI {
   getLastUpdatedAt(token: Token): Promise<number>;
 }
