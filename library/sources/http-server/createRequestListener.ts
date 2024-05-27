d3c66fa2ad0b58d6e0eefba134b92f693349861b
Hans Ott <hansott@hotmail.be>
2024-05-27 17:18:55 +0200

    Move getMaxBodySize() inside request listener
    Otherwise we cannot update this using the config later

--- library/sources/http-server/createRequestListener.ts
+++ library/sources/http-server/createRequestListener.ts
@@ -11,18 +11,15 @@ export function createRequestListener(
   module: string,
   agent: Agent
 ): RequestListener {
-  const maxBodySize = getMaxBodySize();
-
   return async function requestListener(req, res) {
     let body = "";
     let bodySize = 0;
+    const maxBodySize = getMaxBodySize();
     const stream = new PassThrough();
 
     try {
       for await (const chunk of req) {
-        bodySize += chunk.length;
-
-        if (bodySize > maxBodySize) {
+        if (bodySize + chunk.length > maxBodySize) {
           res.statusCode = 413;
           res.write(
             "This request was aborted by Aikido runtime because the body size exceeded the maximum allowed size. Use AIKIDO_MAX_BODY_SIZE_MB to increase the limit."
@@ -32,10 +29,11 @@ export function createRequestListener(
           return;
         }
 
+        bodySize += chunk.length;
         body += chunk.toString();
         stream.push(chunk);
       }
-    } catch (e) {
+    } catch {
       res.statusCode = 500;
       res.write(
         "Aikido runtime encountered an error while reading the request body."
