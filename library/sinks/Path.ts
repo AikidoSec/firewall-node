0498a783c4b9a8dc11590c408a6a94b7fcc83b09
Timo Kössler <info@timokoessler.de>
2024-05-30 17:51:25 +0200

    Extend absolute path traversal comment

--- library/sinks/Path.ts
+++ library/sinks/Path.ts
@@ -17,7 +17,9 @@ export class Path implements Wrapper {
           filename: path,
           operation: `path.${operation}`,
           context: context,
-          // Only check the start of the path for absolute path traversal
+          /* Only check the first arg for absolute path traversal.
+             If a insecure absolute path is passed as the second argument,
+             it can not be an absolute path because it is not the start of the resulting path. */
           checkPathStart: path === args[0],
         });
 
