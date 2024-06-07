aab4a4b93e80c459384dc0ee9abd8826cb8c80b8
Hans Ott <hansott@hotmail.be>
2024-06-07 11:07:30 +0200

    Fix test

--- library/agent/ServiceConfig.test.ts
+++ library/agent/ServiceConfig.test.ts
@@ -1,5 +1,4 @@
 import * as t from "tap";
-import { Endpoints } from "./Endpoints";
 import { ServiceConfig } from "./ServiceConfig";
 
 t.test("it returns false if empty rules", async () => {
@@ -7,7 +6,10 @@ t.test("it returns false if empty rules", async () => {
   t.same(config.getLastUpdatedAt(), 0);
   t.same(config.isUserBlocked("id"), false);
   t.same(config.isAllowedIP("1.2.3.4"), false);
-  t.same(config.getEndpoints(), new Endpoints([]));
+  t.same(
+    config.getEndpoint({ url: undefined, method: undefined, route: undefined }),
+    undefined
+  );
 });
 
 t.test("it works", async () => {
@@ -51,53 +53,25 @@ t.test("it works", async () => {
 
   t.same(config.isUserBlocked("123"), true);
   t.same(config.isUserBlocked("567"), false);
-});
-
-t.test("it returns endpoints", async () => {
-  const config = new ServiceConfig(
-    [
-      {
-        method: "GET",
-        route: "/foo",
-        forceProtectionOff: false,
-        rateLimiting: { enabled: true, maxRequests: 10, windowSizeInMS: 1000 },
-      },
-      {
-        method: "POST",
-        route: "/foo",
-        forceProtectionOff: true,
-        rateLimiting: {
-          enabled: false,
-          maxRequests: 0,
-          windowSizeInMS: 0,
-        },
-      },
-    ],
-    0,
-    [],
-    []
-  );
-
   t.same(
-    config.getEndpoints(),
-    new Endpoints([
-      {
+    config.getEndpoint({
+      url: undefined,
+      method: "GET",
+      route: "/foo",
+    }),
+    {
+      endpoint: {
         method: "GET",
         route: "/foo",
         forceProtectionOff: false,
-        rateLimiting: { enabled: true, maxRequests: 10, windowSizeInMS: 1000 },
-      },
-      {
-        method: "POST",
-        route: "/foo",
-        forceProtectionOff: true,
         rateLimiting: {
           enabled: false,
           maxRequests: 0,
           windowSizeInMS: 0,
         },
       },
-    ])
+      route: "/foo",
+    }
   );
 });
 
