b62afafeb000a64e1fc9c273cca1d978721d81b1
Hans Ott <hansott@hotmail.be>
2024-06-12 16:22:53 +0200

    Improve readability

--- library/sources/http-server/routeExists.ts
+++ library/sources/http-server/routeExists.ts
@@ -1,7 +1,11 @@
 const NOT_FOUND = 404;
 const METHOD_NOT_ALLOWED = 405;
-const CODES = [NOT_FOUND, METHOD_NOT_ALLOWED];
+const ERROR_CODES = [NOT_FOUND, METHOD_NOT_ALLOWED];
 
 export function routeExists(statusCode: number) {
-  return !CODES.includes(statusCode);
+  if (ERROR_CODES.includes(statusCode)) {
+    return false;
+  }
+
+  return true;
 }
