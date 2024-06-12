48fcb841027c2958124400b23b12b73358b824cf
Hans Ott <hansott@hotmail.be>
2024-06-12 16:52:01 +0200

    Detect emails in URL

--- library/helpers/buildRouteFromURL.ts
+++ library/helpers/buildRouteFromURL.ts
@@ -4,6 +4,8 @@ const UUID =
   /(?:[0-9a-f]{8}-[0-9a-f]{4}-[1-8][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}|00000000-0000-0000-0000-000000000000|ffffffff-ffff-ffff-ffff-ffffffffffff)$/i;
 const NUMBER = /^\d+$/;
 const DATE = /^\d{4}-\d{2}-\d{2}|\d{2}-\d{2}-\d{4}$/;
+const EMAIL =
+  /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
 export function buildRouteFromURL(url: string) {
   const path = tryParseURLPath(url);
@@ -36,6 +38,10 @@ function replaceURLSegmentWithParam(segment: string) {
 
   if (DATE.test(segment)) {
     return ":date";
+  }
+
+  if (EMAIL.test(segment)) {
+    return ":email";
   }
 
   return segment;
