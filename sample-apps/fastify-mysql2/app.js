b976a9e378952c21af9e17db3d9c2229b2b04209
Timo Kössler <info@timokoessler.de>
2024-06-16 15:46:16 +0200

    Add fastify sample app

---
+++ sample-apps/fastify-mysql2/app.js
@@ -0,0 +1,49 @@
+// Import Aikido Firewall
+
+require("@aikidosec/firewall");
+const { createConnection } = require("./db");
+const fastify = require("fastify");
+const Cats = require("./Cats");
+
+// Prevent Prototype Pollution
+require("@aikidosec/firewall/nopp");
+
+function getHTMLBody(cats) {
+  return `
+<html lang="en">
+  <body>
+    <p>All cats : ${cats.join(", ")}</p>
+    <form action="/" method="GET">
+      <label for="search">Add a new cat</label>
+      <input type="text" name="petname">
+      <input type="submit" value="Add" />
+    </form>
+    <a href="http://localhost:4000/?petname=Kitty'); DELETE FROM cats;-- H">Test injection</a> / <a href="http://localhost:4000/clear">Clear table</a>
+  </body>
+</html>`;
+}
+
+(async () => {
+  const db = await createConnection();
+  const cats = new Cats(db);
+
+  const app = fastify({
+    logger: true,
+  });
+
+  app.get("/", async (request, reply) => {
+    if (request.query["petname"]) {
+      await cats.add(request.query["petname"]);
+    }
+
+    const html = getHTMLBody(await cats.getAll());
+    reply.header("Content-Type", "text/html").send(html);
+  });
+
+  try {
+    await app.listen({ port: 4000 });
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+})();
