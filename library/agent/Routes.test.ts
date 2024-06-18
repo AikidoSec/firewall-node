9d4964fb11e2c09f11b398f5281f9fad9ca30a6c
Hans Ott <hansott@hotmail.be>
2024-06-18 11:37:40 +0200

    Collect hits for routes/endpoints
    And evict the one with least amount of hits

--- library/agent/Routes.test.ts
+++ library/agent/Routes.test.ts
@@ -6,32 +6,32 @@ t.test("it works", async (t) => {
   t.same(routes.asArray(), []);
 
   routes.addRoute("GET", "/users");
-  t.same(routes.asArray(), [{ method: "GET", path: "/users" }]);
+  t.same(routes.asArray(), [{ method: "GET", path: "/users", hits: 1 }]);
 
   routes.addRoute("GET", "/users");
-  t.same(routes.asArray(), [{ method: "GET", path: "/users" }]);
+  t.same(routes.asArray(), [{ method: "GET", path: "/users", hits: 2 }]);
 
   routes.addRoute("POST", "/users");
   t.same(
     routes.asArray(),
     [
-      { method: "GET", path: "/users" },
-      { method: "POST", path: "/users" },
+      { method: "GET", path: "/users", hits: 2 },
+      { method: "POST", path: "/users", hits: 1 },
     ],
     "Should add second route"
   );
 
   routes.addRoute("PUT", "/users/1");
   t.same(routes.asArray(), [
-    { method: "GET", path: "/users" },
-    { method: "POST", path: "/users" },
-    { method: "PUT", path: "/users/1" },
+    { method: "GET", path: "/users", hits: 2 },
+    { method: "POST", path: "/users", hits: 1 },
+    { method: "PUT", path: "/users/1", hits: 1 },
   ]);
 
   routes.addRoute("DELETE", "/users/1");
   t.same(routes.asArray(), [
-    { method: "POST", path: "/users" },
-    { method: "PUT", path: "/users/1" },
-    { method: "DELETE", path: "/users/1" },
+    { method: "GET", path: "/users", hits: 2 },
+    { method: "PUT", path: "/users/1", hits: 1 },
+    { method: "DELETE", path: "/users/1", hits: 1 },
   ]);
 });
