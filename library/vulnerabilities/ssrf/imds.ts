8771fed0c67a02017783f29661d1e076bf2d96c0
Hans Ott <hansott@hotmail.be>
2024-07-01 12:38:01 +0200

    Add IPv6 for IMDS

--- library/vulnerabilities/ssrf/imds.ts
+++ library/vulnerabilities/ssrf/imds.ts
@@ -2,18 +2,19 @@ import { BlockList } from "net";
 
 const IMDSAddresses = new BlockList();
 
-// This IP address is used by AWS EC2 instances to access the instance metadata service
-// We should block any requests to this IP address
+// This IP address is used by AWS EC2 instances to access the instance metadata service (IMDS)
+// We should block any requests to these IP addresses
 // This prevents STORED SSRF attacks that try to access the instance metadata service
-IMDSAddresses.addAddress("169.254.169.254");
+IMDSAddresses.addAddress("169.254.169.254", "ipv4");
+IMDSAddresses.addAddress("fd00:ec2::254", "ipv6");
 
 export function isIMDSIPAddress(ip: string): boolean {
-  return IMDSAddresses.check(ip);
+  return IMDSAddresses.check(ip) || IMDSAddresses.check(ip, "ipv6");
 }
 
-// Google also uses 169.254.169.254 for its metadata service
+// Google cloud uses the same IP addresses for its metadata service
 // However, you need to set specific headers to access it
-// In order to not block legitimate requests, we should allow the IP address for Google Cloud
+// In order to not block legitimate requests, we should allow the IP addresses for Google Cloud
 const trustedHosts = ["metadata.google.internal", "metadata.goog"];
 
 export function isTrustedHostname(hostname: string): boolean {
