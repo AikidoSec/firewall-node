17cd44c0bc4801c6a8228920d5afa7122b7962bf
Hans Ott <hansott@hotmail.be>
2024-07-01 16:14:29 +0200

    Split function

--- library/vulnerabilities/ssrf/inspectDNSLookupCalls.ts
+++ library/vulnerabilities/ssrf/inspectDNSLookupCalls.ts
@@ -1,137 +1,14 @@
-/* eslint-disable max-lines-per-function */
 import { isIP } from "net";
 import { LookupAddress } from "node:dns";
 import { Agent } from "../../agent/Agent";
 import { attackKindHumanName } from "../../agent/Attack";
-import { getContext } from "../../agent/Context";
+import { Context, getContext } from "../../agent/Context";
 import { Source } from "../../agent/Source";
 import { extractStringsFromUserInput } from "../../helpers/extractStringsFromUserInput";
 import { isPlainObject } from "../../helpers/isPlainObject";
 import { findHostnameInUserInput } from "./findHostnameInUserInput";
 import { isPrivateIP } from "./isPrivateIP";
 import { isIMDSIPAddress, isTrustedHostname } from "./imds";
-
-function wrapDNSLookupCallback(
-  callback: Function,
-  hostname: string,
-  module: string,
-  agent: Agent,
-  operation: string
-): Function {
-  return function wrappedDNSLookupCallback(
-    err: Error,
-    addresses: string | LookupAddress[],
-    family: number
-  ) {
-    if (err) {
-      return callback(err);
-    }
-
-    const context = getContext();
-
-    const resolvedIPAddresses: string[] = [];
-    for (const address of Array.isArray(addresses) ? addresses : [addresses]) {
-      if (typeof address === "string") {
-        resolvedIPAddresses.push(address);
-        continue;
-      }
-
-      if (isPlainObject(address) && address.address) {
-        resolvedIPAddresses.push(address.address);
-      }
-    }
-
-    if (!context) {
-      // Block stored SSRF attack that target IMDS IP addresses
-      // An attacker could have stored a hostname in a database that points to an IMDS IP address
-      // isTrustedHostname is used to allow requests to the Google Cloud metadata service (and other services)
-      // We don't check if the user input contains the IMDS IP address because there's no context
-      const blockedIP = resolvedIPAddresses.find((ip) => isIMDSIPAddress(ip));
-      if (blockedIP && !isTrustedHostname(hostname)) {
-        if (agent.shouldBlock()) {
-          return callback(
-            new Error(
-              `Aikido firewall has blocked ${attackKindHumanName("ssrf")}: ${operation}(...) originating from unknown source`
-            )
-          );
-        }
-      }
-
-      return callback(err, addresses, family);
-    }
-
-    const endpoint = agent.getConfig().getEndpoint(context);
-
-    if (endpoint && endpoint.endpoint.forceProtectionOff) {
-      return callback(err, addresses, family);
-    }
-
-    const privateIP = resolvedIPAddresses.find(isPrivateIP);
-
-    if (!privateIP) {
-      return callback(err, addresses, family);
-    }
-
-    let detected:
-      | {
-          source: Source;
-          pathToPayload: string;
-          payload: string;
-        }
-      | undefined = undefined;
-    for (const source of [
-      "body",
-      "query",
-      "headers",
-      "cookies",
-      "routeParams",
-      "graphql",
-      "xml",
-    ] as Source[]) {
-      if (context[source]) {
-        const userInput = extractStringsFromUserInput(context[source]);
-        for (const [str, path] of userInput.entries()) {
-          const found = findHostnameInUserInput(str, hostname);
-          if (found) {
-            detected = {
-              source: source,
-              pathToPayload: path,
-              payload: str,
-            };
-            break;
-          }
-        }
-      }
-    }
-
-    if (!detected) {
-      return callback(err, addresses, family);
-    }
-
-    agent.onDetectedAttack({
-      module: module,
-      operation: operation,
-      kind: "ssrf",
-      source: detected.source,
-      blocked: agent.shouldBlock(),
-      stack: new Error().stack!,
-      path: detected.pathToPayload,
-      metadata: {},
-      request: context,
-      payload: detected.payload,
-    });
-
-    if (agent.shouldBlock()) {
-      return callback(
-        new Error(
-          `Aikido firewall has blocked ${attackKindHumanName("ssrf")}: ${operation}(...) originating from ${detected.source}${detected.pathToPayload}`
-        )
-      );
-    }
-
-    return callback(err, addresses, family);
-  };
-}
 
 export function inspectDNSLookupCalls(
   lookup: Function,
@@ -178,4 +55,151 @@ export function inspectDNSLookupCalls(
 
     lookup(...argsToApply);
   };
+}
+
+// eslint-disable-next-line max-lines-per-function
+function wrapDNSLookupCallback(
+  callback: Function,
+  hostname: string,
+  module: string,
+  agent: Agent,
+  operation: string
+): Function {
+  // eslint-disable-next-line max-lines-per-function
+  return function wrappedDNSLookupCallback(
+    err: Error,
+    addresses: string | LookupAddress[],
+    family: number
+  ) {
+    if (err) {
+      return callback(err);
+    }
+
+    const context = getContext();
+    const resolvedIPAddresses = getResolvedIPAddresses(addresses);
+
+    if (!context) {
+      if (resolvesToIMDSIP(resolvedIPAddresses, hostname)) {
+        // Block stored SSRF attack that target IMDS IP addresses
+        // An attacker could have stored a hostname in a database that points to an IMDS IP address
+        // We don't check if the user input contains the hostname because there's no context
+        if (agent.shouldBlock()) {
+          return callback(
+            new Error(
+              `Aikido firewall has blocked ${attackKindHumanName("ssrf")}: ${operation}(...) originating from unknown source`
+            )
+          );
+        }
+      }
+
+      return callback(err, addresses, family);
+    }
+
+    const endpoint = agent.getConfig().getEndpoint(context);
+
+    if (endpoint && endpoint.endpoint.forceProtectionOff) {
+      return callback(err, addresses, family);
+    }
+
+    const privateIP = resolvedIPAddresses.find(isPrivateIP);
+
+    if (!privateIP) {
+      return callback(err, addresses, family);
+    }
+
+    const found = findHostnameInContext(hostname, context);
+
+    if (!found) {
+      return callback(err, addresses, family);
+    }
+
+    agent.onDetectedAttack({
+      module: module,
+      operation: operation,
+      kind: "ssrf",
+      source: found.source,
+      blocked: agent.shouldBlock(),
+      stack: new Error().stack!,
+      path: found.pathToPayload,
+      metadata: {},
+      request: context,
+      payload: found.payload,
+    });
+
+    if (agent.shouldBlock()) {
+      return callback(
+        new Error(
+          `Aikido firewall has blocked ${attackKindHumanName("ssrf")}: ${operation}(...) originating from ${found.source}${found.pathToPayload}`
+        )
+      );
+    }
+
+    return callback(err, addresses, family);
+  };
+}
+
+type Location = {
+  source: Source;
+  pathToPayload: string;
+  payload: string;
+};
+
+function findHostnameInContext(
+  hostname: string,
+  context: Context
+): Location | undefined {
+  for (const source of [
+    "body",
+    "query",
+    "headers",
+    "cookies",
+    "routeParams",
+    "graphql",
+    "xml",
+  ] as Source[]) {
+    if (context[source]) {
+      const userInput = extractStringsFromUserInput(context[source]);
+      for (const [str, path] of userInput.entries()) {
+        const found = findHostnameInUserInput(str, hostname);
+        if (found) {
+          return {
+            source: source,
+            pathToPayload: path,
+            payload: str,
+          };
+        }
+      }
+    }
+  }
+
+  return undefined;
+}
+
+function getResolvedIPAddresses(addresses: string | LookupAddress[]): string[] {
+  const resolvedIPAddresses: string[] = [];
+  for (const address of Array.isArray(addresses) ? addresses : [addresses]) {
+    if (typeof address === "string") {
+      resolvedIPAddresses.push(address);
+      continue;
+    }
+
+    if (isPlainObject(address) && address.address) {
+      resolvedIPAddresses.push(address.address);
+    }
+  }
+
+  return resolvedIPAddresses;
+}
+
+function resolvesToIMDSIP(
+  resolvedIPAddresses: string[],
+  hostname: string
+): boolean {
+  // Allow access to Google Cloud metadata service as you need to set specific headers to access it
+  // We don't want to block legitimate requests
+  if (isTrustedHostname(hostname)) {
+    return false;
+  }
+
+  return resolvedIPAddresses.some((ip) => isIMDSIPAddress(ip));
 }
