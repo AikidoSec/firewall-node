6deb026f002a085d77d26c089995fa2ba41c7223
Hans Ott <hansott@hotmail.be>
2024-07-12 18:52:20 +0200

    Cleanup stack trace

--- library/helpers/cleanupStackTrace.ts
+++ library/helpers/cleanupStackTrace.ts
@@ -1,24 +1,79 @@
-import { sep } from "path";
+import { escapeStringRegexp } from "./escapeStringRegexp";
 
+const justACarrot = /^\^$/;
+
+// eslint-disable-next-line max-lines-per-function
 export function cleanupStackTrace(stack: string, libraryRoot: string): string {
+  const pathRegex = new RegExp(escapeStringRegexp(libraryRoot) + "\\S*");
+
+  const pathRegexWithLineNumbers = new RegExp(
+    "^" + escapeStringRegexp(libraryRoot) + "\\S*(:\\d+){1,2}$"
+  );
+
   return stack
     .split("\n")
     .filter((line) => {
+      if (
+        line.includes("cleanupStackTrace(") &&
+        line.includes("new Error().stack!")
+      ) {
+        return false;
+      }
+
+      // /Users/hansott/Code/my-project/server/node_modules/@aikidosec/firewall/agent/applyHooks.js:152
+      if (pathRegexWithLineNumbers.test(line.trimStart())) {
+        return false;
+      }
+
+      // Cleanup carrot under new Error().stack!
+      // stack: cleanupStackTrace(new Error().stack!, libraryRoot),
+      //                           ^
+      if (justACarrot.test(line.trimStart())) {
+        return false;
+      }
+
       if (!line.trimStart().startsWith("at ")) {
         return true;
       }
 
-      // e.g. at Collection.wrap (/Code/runtime-node/build/agent/applyHooks.js:154:75)
-      // e.g. at /Code/runtime-node/build/sources/express/wrapRequestHandler.js:22:20
-      const path = line.match(/\(([^)]+)\)/)?.[1] || line.split(" ").pop();
+      // Cleanup our own stack traces
+      // Examples:
+      // at Collection.wrap (/Code/runtime-node/build/agent/applyHooks.js:154:75)
+      // at /Code/runtime-node/build/sources/express/wrapRequestHandler.js:22:20
+      const parts = line.trimStart().split(" ");
 
-      if (!path || !path.includes(sep)) {
-        return true;
+      if (parts.length > 1) {
+        const lastPart = parts[parts.length - 1];
+
+        if (
+          lastPart.startsWith("(") &&
+          lastPart.endsWith(")") &&
+          pathRegexWithLineNumbers.test(lastPart.slice(1, -1))
+        ) {
+          return false;
+        }
+
+        if (pathRegexWithLineNumbers.test(lastPart)) {
+          return false;
+        }
       }
 
-      const isPartOfFirewall = path.startsWith(libraryRoot);
-
-      return !isPartOfFirewall;
+      return true;
     })
-    .join("\n");
+    .map((line) => {
+      if (line.trimStart().startsWith("at ")) {
+        const parts = line.trimStart().split(" ");
+        if (parts.length === 4) {
+          const lastPart = parts[parts.length - 1];
+          // Cleanup our own stack traces
+          // Examples
+          // at Object.unifiedUsers (/Users/hansott/Code/my-project/server/src/GraphQL/Mutation.ts:4491:31) /Users/hansott/Code/my-project/server/node_modules/@aikidosec/firewall
+          return line.replace(pathRegex, "");
+        }
+      }
+
+      return line;
+    })
+    .join("\n")
+    .trim();
 }
