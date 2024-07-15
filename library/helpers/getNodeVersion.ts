14546ad1c9c7a28b347cab0729515b13b2e91a51
Hans Ott <hansott@hotmail.be>
2024-07-15 16:54:22 +0200

    Undici is supported from 16.8

--- library/helpers/getNodeVersion.ts
+++ library/helpers/getNodeVersion.ts
@@ -1,3 +1,7 @@
 export function getMajorNodeVersion(): number {
   return parseInt(process.version.split(".")[0].slice(1));
 }
+
+export function getMinorNodeVersion() {
+  return parseInt(process.version.split(".")[1]);
+}
