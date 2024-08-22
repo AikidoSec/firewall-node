c3721befbd8457fe34924026579ff994284c0279
Timo Kössler <info@timokoessler.de>
2024-08-22 17:49:52 +0200

    Add nestjs-sentry sample-app

---
+++ sample-apps/nestjs-sentry/src/main.ts
@@ -0,0 +1,27 @@
+import '@aikidosec/firewall';
+import * as Sentry from '@sentry/nestjs';
+
+Sentry.init({
+  dsn: 'https://examplePublicKey@o0.ingest.sentry.io/0',
+});
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+function getPort() {
+  const port = parseInt(process.env.PORT, 10) || 4000;
+
+  if (isNaN(port)) {
+    console.error('Invalid port');
+    process.exit(1);
+  }
+
+  return port;
+}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(getPort());
+}
+
+bootstrap();
