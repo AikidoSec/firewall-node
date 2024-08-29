e29874e00cb2d17334afa9c113f95ba50e2eec70
Hans Ott <hansott@hotmail.be>
2024-08-29 14:33:42 +0200

    Update test

--- library/vulnerabilities/sql-injection/tokenize.test.ts
+++ library/vulnerabilities/sql-injection/tokenize.test.ts
@@ -3,20 +3,20 @@ import * as t from "tap";
 import { tokenize } from "./tokenize";
 
 t.test("it tokenizes query", async () => {
-  const query = "SELECT * FROM users WHERE name = 'John';";
+  const query = `SELECT * FROM users WHERE name = 'John';`;
   const tokens = tokenize("mysql", query);
   const expected = [
-    { Word: { value: "SELECT", keyword: "SELECT", quote_style: undefined } },
+    { Word: { value: "SELECT", quote_style: undefined, keyword: "SELECT" } },
     { Whitespace: "Space" },
     "Mul",
     { Whitespace: "Space" },
-    { Word: { value: "FROM", keyword: "FROM", quote_style: undefined } },
+    { Word: { value: "FROM", quote_style: undefined, keyword: "FROM" } },
     { Whitespace: "Space" },
-    { Word: { value: "users", keyword: "NoKeyword", quote_style: undefined } },
+    { Word: { value: "users", quote_style: undefined, keyword: "NoKeyword" } },
     { Whitespace: "Space" },
-    { Word: { value: "WHERE", keyword: "WHERE", quote_style: undefined } },
+    { Word: { value: "WHERE", quote_style: undefined, keyword: "WHERE" } },
     { Whitespace: "Space" },
-    { Word: { value: "name", keyword: "NAME", quote_style: undefined } },
+    { Word: { value: "name", quote_style: undefined, keyword: "NAME" } },
     { Whitespace: "Space" },
     "Eq",
     { Whitespace: "Space" },
