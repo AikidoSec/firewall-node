6cdb851f7f7a0d5005c49a4f26fbe9b6ee095242
Timo Kössler <info@timokoessler.de>
2024-09-03 10:34:18 +0200

    Update dev dependencies to fix security warnings

--- library/agent/applyHooks.test.ts
+++ library/agent/applyHooks.test.ts
@@ -142,7 +142,7 @@ t.test("it adds try/catch around the wrapped method", async (t) => {
   const { agent, logger } = createAgent();
   t.same(applyHooks(hooks, agent), {
     mysql2: {
-      version: "3.10.3",
+      version: "3.11.0",
       supported: true,
     },
   });
