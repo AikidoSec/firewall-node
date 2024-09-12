d9b9be667bb01b060f3b01314e2f3dd341c59f50
Timo Kössler <info@timokoessler.de>
2024-09-12 12:44:46 +0200

    Only sample first 20 bodies for api discovery

--- benchmarks/api-discovery/reqBodies.js
+++ benchmarks/api-discovery/reqBodies.js
@@ -181,4 +181,42 @@ module.exports = [
       },
     ],
   },
+  {
+    user: {
+      id: 5,
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      address: {
+        street: "123 Main St",
+        city: "New York",
+        zip: "13245",
+        country: "USA",
+      },
+      preferences: [1, 2, 3],
+    },
+    feedback: {
+      feedbackType: "bug_report",
+      description:
+        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,",
+      metadata: {
+        appVersion: "1.0.0",
+        device: "Android",
+        osVer: 14,
+      },
+    },
+    filters: {
+      subobject: {
+        key: "value",
+        nested: {
+          key: "value",
+          array: [
+            {
+              key: "value",
+              nested: { key: "value" },
+            },
+          ],
+        },
+      },
+    },
+  },
 ];
