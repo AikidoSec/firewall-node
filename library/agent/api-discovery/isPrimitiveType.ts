5990acb94f63f8ebc9149d2e0caa71153b258ec7
Hans Ott <hansott@hotmail.be>
2024-09-16 17:05:12 +0200

    Don't export

--- library/agent/api-discovery/isPrimitiveType.ts
+++ library/agent/api-discovery/isPrimitiveType.ts
@@ -1,10 +1,10 @@
-export function isPrimitiveType(type: string): boolean {
-  return !["object", "array"].includes(type);
-}
-
 export function onlyContainsPrimitiveTypes(types: string | string[]): boolean {
   if (!Array.isArray(types)) {
     return isPrimitiveType(types);
   }
   return types.every(isPrimitiveType);
+}
+
+function isPrimitiveType(type: string): boolean {
+  return !["object", "array"].includes(type);
 }
