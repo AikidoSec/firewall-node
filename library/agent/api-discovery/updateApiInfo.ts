be3ea9fb552290c064f54f9345c5fd1d749c0bdf
Timo Kössler <info@timokoessler.de>
2024-09-17 15:24:47 +0200

    Add try/catch to be sure

--- library/agent/api-discovery/updateApiInfo.ts
+++ library/agent/api-discovery/updateApiInfo.ts
@@ -8,40 +8,44 @@ import { mergeDataSchemas } from "./mergeDataSchemas";
  * Updates the body, query, and auth info of an existing route with new info from the context.
  */
 export function updateApiInfo(context: Context, existingRoute: Route): void {
-  const {
-    body: newBody,
-    query: newQuery,
-    auth: newAuth,
-  } = getApiInfo(context) || {};
+  try {
+    const {
+      body: newBody,
+      query: newQuery,
+      auth: newAuth,
+    } = getApiInfo(context) || {};
 
-  const existingSpec = existingRoute.apispec;
+    const existingSpec = existingRoute.apispec;
 
-  // Merge body schemas if both exists, otherwise set the new body schema if it exists
-  if (existingSpec.body && newBody) {
-    existingSpec.body = {
-      type: newBody.type,
-      schema: mergeDataSchemas(existingSpec.body?.schema, newBody.schema),
-    };
-  } else if (newBody) {
-    existingSpec.body = newBody;
-  }
-
-  if (
-    newQuery &&
-    typeof newQuery === "object" &&
-    Object.keys(newQuery).length > 0
-  ) {
-    if (existingSpec.query && newQuery) {
-      existingSpec.query = mergeDataSchemas(existingSpec.query, newQuery);
-    } else {
-      existingSpec.query = newQuery;
+    // Merge body schemas if both exists, otherwise set the new body schema if it exists
+    if (existingSpec.body && newBody) {
+      existingSpec.body = {
+        type: newBody.type,
+        schema: mergeDataSchemas(existingSpec.body?.schema, newBody.schema),
+      };
+    } else if (newBody) {
+      existingSpec.body = newBody;
     }
-  }
 
-  existingSpec.auth = mergeApiAuthTypes(existingSpec.auth, newAuth);
+    if (
+      newQuery &&
+      typeof newQuery === "object" &&
+      Object.keys(newQuery).length > 0
+    ) {
+      if (existingSpec.query && newQuery) {
+        existingSpec.query = mergeDataSchemas(existingSpec.query, newQuery);
+      } else {
+        existingSpec.query = newQuery;
+      }
+    }
 
-  // Normalize empty apispec so we do not get something like { auth: undefined }
-  if (!existingSpec.body && !existingSpec.query && !existingSpec.auth) {
-    existingRoute.apispec = {};
+    existingSpec.auth = mergeApiAuthTypes(existingSpec.auth, newAuth);
+
+    // Normalize empty apispec so we do not get something like { auth: undefined }
+    if (!existingSpec.body && !existingSpec.query && !existingSpec.auth) {
+      existingRoute.apispec = {};
+    }
+  } catch {
+    //
   }
 }
