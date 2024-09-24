671bfe9137e83bb72970b83bbacd98e2b91d3c06
Timo Kössler <info@timokoessler.de>
2024-09-24 18:44:37 +0200

    Add distinct test with safe context

--- library/sinks/MongoDB.test.ts
+++ library/sinks/MongoDB.test.ts
@@ -107,6 +107,13 @@ t.test("it inspects method calls and blocks if needed", async (t) => {
       "Yet Another Title",
     ]);
 
+    // With context
+    await runWithContext(safeContext, async () => {
+      t.same(await collection.distinct("title", { title: { $ne: null } }), [
+        "Yet Another Title",
+      ]);
+    });
+
     await collection.deleteOne({ title: "Yet Another Title" });
 
     t.same(await collection.count({ title: "Yet Another Title" }), 0);
