a59f022b07c20b122b8138a529cb03e7452c2c78
Timo Kössler <info@timokoessler.de>
2024-09-26 09:50:37 +0200

    Fix failing test

--- library/helpers/getPackageVersion.test.ts
+++ library/helpers/getPackageVersion.test.ts
@@ -4,5 +4,5 @@ import { getPackageVersion } from "./getPackageVersion";
 t.test("it resolves the version of a package", async (t) => {
   t.same(getPackageVersion("express"), "5.0.0");
   t.same(getPackageVersion("non-existing-package"), null);
-  t.same(getPackageVersion("@google-cloud/functions-framework"), "3.4.1");
+  t.same(getPackageVersion("@google-cloud/functions-framework"), "3.4.2");
 });
