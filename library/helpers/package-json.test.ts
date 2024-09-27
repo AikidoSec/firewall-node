e1d55bb46a545dc32545afe0dde0d711a6c3181e
Hans Ott <hansott@hotmail.be>
2024-09-27 14:22:36 +0200

    Fix dependency test

--- library/helpers/package-json.test.ts
+++ library/helpers/package-json.test.ts
@@ -2,6 +2,6 @@ import * as t from "tap";
 
 const PackageJson = require("../package.json");
 
-t.test("Check that no dependencies are present", async (t) => {
-  t.equal(PackageJson.dependencies, undefined);
+t.test("Check that no other dependencies are present", async (t) => {
+  t.same(Object.keys(PackageJson.dependencies), ["@aikidosec/zen-internals"]);
 });
