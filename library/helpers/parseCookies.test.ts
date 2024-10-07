e7a76e35afae3055018b0519a538a86eb674c618
Timo Kössler <info@timokoessler.de>
2024-10-07 16:23:56 +0200

    Update library/helpers/parseCookies.test.ts
    Co-authored-by: Hans Ott <hansott@hotmail.be>

--- library/helpers/parseCookies.test.ts
+++ library/helpers/parseCookies.test.ts
@@ -52,3 +52,18 @@ t.test("should ignore duplicate cookies", async () => {
 t.test("empty string should return empty object", async () => {
   t.same(parse(""), {});
 });
+
+tap.test('should parse quoted values', async (t) => {
+  t.same(parse('foo="bar"'), { foo: 'bar' });
+  t.same(parse('foo=" a b c "'), { foo: ' a b c ' });
+});
+
+tap.test('should trim whitespace around key and value', async (t) => {
+  t.same(parse('  foo  =  "bar"  '), { foo: 'bar' });
+  t.same(parse('  foo  =  bar  ;  fizz  =  buzz  '), { foo: 'bar', fizz: 'buzz' });
+  t.same(parse(' foo = " a b c " '), { foo: ' a b c ' });
+  t.same(parse(' = bar '), { '': 'bar' });
+  t.same(parse(' foo = '), { foo: '' });
+  t.same(parse('   =   '), { '': '' });
+  t.same(parse('\tfoo\t=\tbar\t'), { foo: 'bar' });
+});
