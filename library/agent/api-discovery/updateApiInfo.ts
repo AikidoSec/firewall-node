0690356bf196907f5b3578b55ff0c694d03c7231
Timo Kössler <info@timokoessler.de>
2024-10-14 16:13:31 +0200

    Move getMaxApiDiscoverySamples call

--- library/agent/api-discovery/updateApiInfo.ts
+++ library/agent/api-discovery/updateApiInfo.ts
@@ -5,13 +5,13 @@ import { getApiInfo } from "./getApiInfo";
 import { mergeApiAuthTypes } from "./mergeApiAuthTypes";
 import { mergeDataSchemas } from "./mergeDataSchemas";
 
-const maxSamples = getMaxApiDiscoverySamples();
-
 /**
  * Updates the body, query, and auth info of an existing route with new info from the context.
  * Only the first 10 hits of a route during one heartbeat window are sampled.
  */
 export function updateApiInfo(context: Context, existingRoute: Route): void {
+  const maxSamples = getMaxApiDiscoverySamples();
+
   // Only sample first x hits of a route during one heartbeat window
   // Default is 10
   if (existingRoute.hits > maxSamples) {
