adbd80748d07612d4f6e045785e5a02410c892b2
Timo Kössler <info@timokoessler.de>
2024-10-18 14:13:51 +0200

    Remove global rate limiting, update express

--- library/middleware/express.ts
+++ library/middleware/express.ts
@@ -1,5 +1,6 @@
 import type { RequestHandler } from "express";
 import { shouldBlockRequest } from ".";
+import { escapeHTML } from "../helpers/escapeHTML";
 
 export function express(): RequestHandler {
   return (req, res, next) => {
@@ -8,15 +9,18 @@ export function express(): RequestHandler {
     if (result.block) {
       if (result.type === "rate-limit") {
         let message = "You are rate limited by Aikido firewall.";
-        if (result.trigger === "ip") {
-          message += ` (Your IP: ${result.ip})`;
+        if (result.trigger === "ip" && result.ip) {
+          message += ` (Your IP: ${escapeHTML(result.ip)})`;
         }
 
-        return res.status(429).send(message);
+        return res.status(429).type("text").send(message);
       }
 
       if (result.type === "blocked") {
-        return res.status(403).send("You are blocked by Aikido firewall.");
+        return res
+          .status(403)
+          .type("text")
+          .send("You are blocked by Aikido firewall.");
       }
     }
 
