9f9f8b0aaf50ab9367b5344adbb8dfe9a946f585
Timo Kössler <info@timokoessler.de>
2024-10-21 10:14:18 +0200

    Cleanup unnecessary code

--- library/sources/hapi/wrapRequestHandler.ts
+++ library/sources/hapi/wrapRequestHandler.ts
@@ -1,5 +1,5 @@
 import type { Lifecycle } from "@hapi/hapi";
-import { getContext, runWithContext } from "../../agent/Context";
+import { runWithContext } from "../../agent/Context";
 import { contextFromRequest } from "./contextFromRequest";
 
 export function wrapRequestHandler(
@@ -9,19 +9,6 @@ export function wrapRequestHandler(
     const context = contextFromRequest(request);
 
     return runWithContext(context, () => {
-      // Even though we already have the context, we need to get it again
-      // The context from `contextFromRequest` will never return a user
-      // The user will be carried over from the previous context
-      const context = getContext();
-
-      if (!context) {
-        return handler.apply(
-          // @ts-expect-error We don't now the type of this
-          this,
-          [request, h]
-        );
-      }
-
       return handler.apply(
         // @ts-expect-error We don't now the type of this
         this,
