b505e399c84ea94a2290c5cda1d09a886e80f359
Hans Ott <hansott@hotmail.be>
2024-10-31 12:19:49 +0100

    Cutoff email at 100 chars

--- library/agent/api-discovery/helpers/isEmail.ts
+++ library/agent/api-discovery/helpers/isEmail.ts
@@ -9,10 +9,9 @@ const emailUserUtf8Part =
   /^[a-z\d!#\$%&'\*\+\-\/=\?\^_`{\|}~\u00A1-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+$/i;
 const quotedEmailUserUtf8 =
   /^([\s\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|(\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*$/i;
-const defaultMaxEmailLength = 254;
 
 export default function isEmailString(str: string): boolean {
-  if (str.length > defaultMaxEmailLength) {
+  if (str.length > 100) {
     return false;
   }
 
