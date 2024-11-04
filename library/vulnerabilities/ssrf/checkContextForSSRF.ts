31e3978d101186bd8103c2d2bbfdbd361fb143cc
Hans Ott <hansott@hotmail.be>
2024-11-04 17:14:28 +0100

    Add comment

--- library/vulnerabilities/ssrf/checkContextForSSRF.ts
+++ library/vulnerabilities/ssrf/checkContextForSSRF.ts
@@ -21,6 +21,10 @@ export function checkContextForSSRF({
   operation: string;
   context: Context;
 }): InterceptorResult {
+  // If the hostname is a private IP address, we don't need to check for SSRF
+  // DNS lookup calls will be inspected somewhere else
+  // This is just to inspect direct invocations of `http.request` and similar
+  // Where the hostname might be a private IP address (or localhost)
   if (!containsPrivateIPAddress(hostname)) {
     return;
   }
