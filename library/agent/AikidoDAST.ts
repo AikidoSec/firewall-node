c552edd9d4f4a6ad7f6229d2e32f533b8c859e37
Hans Ott <hansott@hotmail.be>
2024-11-05 13:58:22 +0100

    Check if headers is an object for safety

--- library/agent/AikidoDAST.ts
+++ library/agent/AikidoDAST.ts
@@ -1,7 +1,12 @@
+import { isPlainObject } from "../helpers/isPlainObject";
 import { Context } from "./Context";
 
 const AIKIDO_DAST_HEADER = "aikido-api-test";
 
 export function isAikidoDASTRequest(context: Context) {
-  return context.headers[AIKIDO_DAST_HEADER] === "1";
+  return (
+    isPlainObject(context.headers) &&
+    AIKIDO_DAST_HEADER in context.headers &&
+    context.headers[AIKIDO_DAST_HEADER] === "1"
+  );
 }
