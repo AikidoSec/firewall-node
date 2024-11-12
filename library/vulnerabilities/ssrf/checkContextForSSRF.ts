7e8d68f7b5cd2f331046c700d0d6c5ada01cde64
Hans Ott <hansott@hotmail.be>
2024-11-12 09:27:26 +0100

    Update comment

--- library/vulnerabilities/ssrf/checkContextForSSRF.ts
+++ library/vulnerabilities/ssrf/checkContextForSSRF.ts
@@ -40,7 +40,7 @@ export function checkContextForSSRF({
           })
         ) {
           // Application might do a request to itself when the hostname is localhost
-          // Let's allow this (only for the headers.host source)
+          // Let's allow this for the following headers: Host, Origin, Referrer
           // We still want to block if the port is different
           continue;
         }
