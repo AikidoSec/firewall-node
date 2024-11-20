bae368d3d78e0bfcd482d18b866ae71ab8571fe0
Hans Ott <hansott@hotmail.be>
2024-11-20 11:58:31 +0100

    Protect against infinite loops in `getRedirectOrigin`

--- library/vulnerabilities/ssrf/getRedirectOrigin.ts
+++ library/vulnerabilities/ssrf/getRedirectOrigin.ts
@@ -1,6 +1,7 @@
 import { Context } from "../../agent/Context";
+
 /**
- * This function checks if the given URL is part of a redirect chain that is passed in the redirects parameter.
+ * This function checks if the given URL is part of a redirect chain that is passed in the `redirects` parameter.
  * It returns the origin of a redirect chain if the URL is the result of a redirect.
  * The origin is the first URL in the chain, so the initial URL that was requested and redirected to the given URL
  * or in case of multiple redirects the URL that was redirected to the given URL.
@@ -13,24 +14,39 @@ import { Context } from "../../agent/Context";
 export function getRedirectOrigin(
   redirects: Context["outgoingRequestRedirects"],
   url: URL
-) {
+): URL | undefined {
   if (!Array.isArray(redirects)) {
     return undefined;
   }
 
-  let currentUrl = url;
+  // Keep track of visited URLs to avoid infinite loops
+  const visited: Set<string> = new Set();
+  let currentUrl: URL = url;
+  let foundRedirect: boolean;
 
-  // Follow the redirect chain until we reach the origin or don't find a redirect
-  while (true) {
-    const redirect = redirects.find(
-      // url.href contains the full URL so we can use it for comparison
-      (r) => r.destination.href === currentUrl.href
+  do {
+    foundRedirect = false;
+
+    // Filter redirects that have not been visited
+    const unvisitedRedirects = redirects.filter(
+      (r) => !visited.has(r.destination.href)
     );
-    if (!redirect) {
+
+    // If no unvisited redirects are left, exit the loop
+    if (unvisitedRedirects.length === 0) {
       break;
     }
-    currentUrl = redirect.source;
-  }
 
+    for (const redirect of unvisitedRedirects) {
+      if (redirect.destination.href === currentUrl.href) {
+        visited.add(redirect.destination.href);
+        currentUrl = redirect.source;
+        foundRedirect = true;
+        break;
+      }
+    }
+  } while (foundRedirect);
+
+  // If the final URL is the same as the starting URL, return undefined (no redirect origin)
   return currentUrl.href === url.href ? undefined : currentUrl;
 }
