5ffd76d30cadcd46f054a0f072e135e309ec8a7f
Hans Ott <hansott@hotmail.be>
2024-11-25 09:55:28 +0100

    Output whether blocking or detection-only mode is enabled

--- library/agent/Agent.ts
+++ library/agent/Agent.ts
@@ -218,9 +218,13 @@ export class Agent {
       if (typeof response.block === "boolean") {
         if (response.block !== this.block) {
           this.block = response.block;
-          this.logger.log(
-            `Block mode has been set to ${this.block ? "on" : "off"}`
-          );
+          if (this.block) {
+            this.logger.log("Blocking mode enabled, requests will be blocked!");
+          } else {
+            this.logger.log(
+              "Detection-only mode enabled, no requests will be blocked!"
+            );
+          }
         }
       }
 
@@ -394,8 +398,12 @@ export class Agent {
 
     this.logger.log("Starting agent...");
 
-    if (!this.block) {
-      this.logger.log("Dry mode enabled, no requests will be blocked!");
+    if (this.block) {
+      this.logger.log("Blocking mode enabled, requests will be blocked!");
+    } else {
+      this.logger.log(
+        "Detection-only mode enabled, no requests will be blocked!"
+      );
     }
 
     if (this.token) {
