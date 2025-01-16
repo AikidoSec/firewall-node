4bec97657e19d9ac01c1a0442612f167b74db86c
Hans Ott <hansott@hotmail.be>
2025-01-16 15:30:32 +0100

    More accurate benchmark for http.request(...)

--- benchmarks/operations/http-request.js
+++ benchmarks/operations/http-request.js
@@ -2,19 +2,10 @@ const http = require("http");
 
 module.exports = {
   step: async function step() {
-    return new Promise((resolve, reject) => {
-      const options = {
-        hostname: "localhost",
-        port: 10411,
-      };
-
-      const req = http.request(options, (res) => {
-        res.on("data", () => {});
-        res.on("end", resolve);
-      });
-
-      req.on("error", reject);
-      req.end();
+    const req = http.request("http://localhost:10411", (res) => {
+      res.resume();
     });
+
+    req.end();
   },
 };
