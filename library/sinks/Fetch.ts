6477fcdc096c090fa77d2b8bd73c033f4282a1d4
Timo Kössler <info@timokoessler.de>
2025-01-20 09:44:57 +0100

    Fix linting

--- library/sinks/Fetch.ts
+++ library/sinks/Fetch.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 import { lookup } from "dns";
 import { Agent } from "../agent/Agent";
 import { getContext } from "../agent/Context";
@@ -37,6 +38,7 @@ export class Fetch implements Wrapper {
 
   inspectFetch(args: unknown[], agent: Agent): InterceptorResult {
     if (args.length > 0) {
+      // URL string
       if (typeof args[0] === "string" && args[0].length > 0) {
         const url = tryParseURL(args[0]);
         if (url) {
@@ -69,6 +71,7 @@ export class Fetch implements Wrapper {
         }
       }
 
+      // URL object
       if (args[0] instanceof URL && args[0].hostname.length > 0) {
         const attack = this.inspectHostname(
           agent,
@@ -80,6 +83,7 @@ export class Fetch implements Wrapper {
         }
       }
 
+      // Request object
       if (args[0] instanceof Request) {
         const url = tryParseURL(args[0].url);
         if (url) {
