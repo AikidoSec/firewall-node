2b1e69804ebfd46dd9129f8d37e483ebaa1adda2
Hans Ott <hansott@hotmail.be>
2025-02-05 15:45:06 +0100

    Move outside if

--- library/helpers/tryParseURL.ts
+++ library/helpers/tryParseURL.ts
@@ -12,18 +12,18 @@ function parseAndCatchError(url: string): URL | undefined {
   }
 }
 
+function canParse(url: string): URL | undefined {
+  if (URL.canParse(url)) {
+    return new URL(url);
+  }
+
+  return undefined;
+}
+
 // URL.canParse(...) is a lot faster than using the constructor and catching the error
 // Added in Node.js: v19.9.0, v18.17.0
 if (typeof URL.canParse === "function") {
   tryParseURL = canParse;
-
-  function canParse(url: string): URL | undefined {
-    if (URL.canParse(url)) {
-      return new URL(url);
-    }
-
-    return undefined;
-  }
 }
 
 export { tryParseURL };
