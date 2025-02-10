4d3d39c6f14554bfcfa18c1a913a6f5b09ead846
Timo Kössler <info@timokoessler.de>
2025-02-10 17:05:47 +0100

    Try to fix e2e tests

--- end2end/tests/hono-sqlite3-esm.test.js
+++ end2end/tests/hono-sqlite3-esm.test.js
@@ -10,28 +10,23 @@ t.test("it logs esm warnings", (t) => {
   const server = spawn(`node`, [pathToApp, "4004"], {
     env: { ...process.env, AIKIDO_DEBUG: "true", AIKIDO_BLOCKING: "true" },
     cwd: appDir,
-    shell: true,
   });
 
   server.on("close", () => {
-    console.log("Server closed, t.end()");
     t.end();
   });
 
   server.on("error", (err) => {
-    console.log("Server error", err);
     t.fail(err.message);
   });
 
   let stdout = "";
   server.stdout.on("data", (data) => {
-    console.log(data.toString());
     stdout += data.toString();
   });
 
   let stderr = "";
   server.stderr.on("data", (data) => {
-    console.log(data.toString());
     stderr += data.toString();
   });
 
@@ -60,22 +55,17 @@ t.test("it logs esm warnings", (t) => {
     .then(([sqlInjection, normalAdd]) => {
       t.equal(sqlInjection.status, 200); // Not blocked
       t.equal(normalAdd.status, 200);
-      console.log("Matching stdout and stderr");
       t.match(
         stderr,
         /Your application seems to be running in ESM mode\. Zen does not support ESM at runtime yet\./
       );
-      console.log("Matching starting agent");
       t.match(stdout, /Starting agent/);
-      console.log("Matching SQL injection");
       t.notMatch(stderr, /Zen has blocked an SQL injection/); // Not supported
-      console.log("Finished matching");
     })
     .catch((error) => {
       t.fail(error.message);
     })
     .finally(() => {
-      console.log("Killing server");
       server.kill();
     });
 });
