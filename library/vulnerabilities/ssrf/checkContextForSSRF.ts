0c7cc483fd6198cc8264ac87f866d7753953d246
Hans Ott <hansott@hotmail.be>
2025-02-27 16:19:49 +0100

    Only ignore base URL if proxy is trusted

--- library/vulnerabilities/ssrf/checkContextForSSRF.ts
+++ library/vulnerabilities/ssrf/checkContextForSSRF.ts
@@ -5,6 +5,7 @@ import { SOURCES } from "../../agent/Source";
 import { getPathsToPayload } from "../../helpers/attackPath";
 import { extractStringsFromUserInputCached } from "../../helpers/extractStringsFromUserInputCached";
 import { getPortFromURL } from "../../helpers/getPortFromURL";
+import { trustProxy } from "../../helpers/trustProxy";
 import { tryParseURL } from "../../helpers/tryParseURL";
 import { containsPrivateIPAddress } from "./containsPrivateIPAddress";
 import { findHostnameInUserInput } from "./findHostnameInUserInput";
@@ -33,10 +34,11 @@ export function checkContextForSSRF({
     return;
   }
 
-  if (context.url) {
+  if (trustProxy() && context.url) {
     // We don't want to block outgoing requests to the same host as the server
     // (often happens that we have a match on headers like `Host`, `Origin`, `Referer`, etc.)
     // We have to check the port as well, because the hostname can be the same but with a different port
+    // If Node.js is exposed to the internet, we can't be sure about the Host header
     const baseURL = tryParseURL(context.url);
     if (
       baseURL &&
--- library/vulnerabilities/ssrf/findHostnameInContext.ts
+++ library/vulnerabilities/ssrf/findHostnameInContext.ts
@@ -3,6 +3,7 @@ import { Source, SOURCES } from "../../agent/Source";
 import { getPathsToPayload } from "../../helpers/attackPath";
 import { extractStringsFromUserInputCached } from "../../helpers/extractStringsFromUserInputCached";
 import { getPortFromURL } from "../../helpers/getPortFromURL";
+import { trustProxy } from "../../helpers/trustProxy";
 import { tryParseURL } from "../../helpers/tryParseURL";
 import { findHostnameInUserInput } from "./findHostnameInUserInput";
 
@@ -19,10 +20,11 @@ export function findHostnameInContext(
   context: Context,
   port: number | undefined
 ): HostnameLocation | undefined {
-  if (context.url) {
+  if (trustProxy() && context.url) {
     // We don't want to block outgoing requests to the same host as the server
     // (often happens that we have a match on headers like `Host`, `Origin`, `Referer`, etc.)
     // We have to check the port as well, because the hostname can be the same but with a different port
+    // If Node.js is exposed to the internet, we can't be sure about the Host header
     const baseURL = tryParseURL(context.url);
     if (
       baseURL &&
