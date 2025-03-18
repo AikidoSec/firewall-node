51db4e9ccc362efa53b311ad77396a725f318595
Hans Ott <hansott@hotmail.be>
2025-03-18 17:55:26 +0100

    Skip end2end test in GitHub actions (add unit test to cover)
        Error: listen EACCES: permission denied 0.0.0.0:443
            at __node_internal_captureLargerStackTrace (node:internal/errors:496:5)
            at __node_internal_uvExceptionWithHostPort (node:internal/errors:593:12)
            at Server.setupListenHandle [as _listen2] (node:net:1800:21)
            at listenInCluster (node:net:1865:12)
            at Server.listen (node:net:1953:7)
            at /home/runner/work/firewall-node/firewall-node/sample-apps/express-http-https/app.js:60:11
            at new Promise (<anonymous>)
            at main (/home/runner/work/firewall-node/firewall-node/sample-apps/express-http-https/app.js:52:5)
            at Object.<anonymous> (/home/runner/work/firewall-node/firewall-node/sample-apps/express-http-https/app.js:71:1)
            at Module._compile (node:internal/modules/cjs/loader:1364:14)
            at Module._compile (/home/runner/work/firewall-node/firewall-node/end2end/node_modules/pirates/lib/index.js:117:24)
            at Module._extensions..js (node:internal/modules/cjs/loader:1422:10)
            at Object.newLoader [as .js] (/home/runner/work/firewall-node/firewall-node/end2end/node_modules/pirates/lib/index.js:121:7)
            at Module.load (node:internal/modules/cjs/loader:1203:32)
            at Module._load (node:internal/modules/cjs/loader:1019:12)
            at ModuleWrap.<anonymous> (node:internal/modules/esm/translators:203:29)
            at ModuleJob.run (node:internal/modules/esm/module_job:195:25)
            at async ModuleLoader.import (node:internal/modules/esm/loader:337:24)
            at async loadESM (node:internal/process/esm_loader:34:7)
            at async handleMainPromise (node:internal/modules/run_main:106:12)
        Emitted 'error' event on Server instance at:
            at emitErrorNT (node:net:1844:8)
            at process.processTicksAndRejections (node:internal/process/task_queues:82:21) {
          code: 'EACCES',
          errno: -13,
          syscall: 'listen',
          address: '0.0.0.0',
          port: 443
        }

--- end2end/tests/express-http-https.test.js
+++ end2end/tests/express-http-https.test.js
@@ -9,6 +9,8 @@ const pathToApp = resolve(
   "app.js"
 );
 
+const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
+
 // This sample app contains the following endpoint:
 // app.get(
 //     "/request",
@@ -22,83 +24,87 @@ const pathToApp = resolve(
 // We want to check if the agent never blocks requests to itself
 // With the special case that HTTP/HTTPS is still allowed
 // But requests to other ports are blocked
-t.test("it blocks in blocking mode", (t) => {
-  const server = spawn(`node`, [pathToApp], {
-    env: {
-      ...process.env,
-      AIKIDO_DEBUG: "true",
-      AIKIDO_BLOCKING: "true",
-      // Required for self-signed certificates
-      // This is to allow the sample app to make insecure requests
-      NODE_TLS_REJECT_UNAUTHORIZED: "0",
-    },
-  });
-
-  // Required for self-signed certificates
-  // See fetch(...) calls below
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
-  server.on("close", () => {
-    t.end();
-  });
-
-  server.on("error", (err) => {
-    t.fail(err);
-  });
-
-  let stdout = "";
-  server.stdout.on("data", (data) => {
-    stdout += data.toString();
-    console.log(data.toString());
-  });
-
-  let stderr = "";
-  server.stderr.on("data", (data) => {
-    stderr += data.toString();
-    console.log(data.toString());
-  });
-
-  // Wait for the server to start
-  timeout(2000)
-    .then(() => {
-      return Promise.all([
-        fetch(
-          `http://localhost/request?url=${encodeURIComponent("https://localhost")}`,
-          {
-            signal: AbortSignal.timeout(5000),
-          }
-        ),
-        fetch(
-          `https://localhost/request?url=${encodeURIComponent("http://localhost")}`,
-          {
-            signal: AbortSignal.timeout(5000),
-          }
-        ),
-        fetch(
-          `http://localhost/request?url=${encodeURIComponent("http://localhost:42313")}`,
-          {
-            signal: AbortSignal.timeout(5000),
-          }
-        ),
-        fetch(
-          `https://localhost/request?url=${encodeURIComponent("http://localhost:42313")}`,
-          {
-            signal: AbortSignal.timeout(5000),
-          }
-        ),
-      ]);
-    })
-    .then(([safe1, safe2, unsafe1, unsafe2]) => {
-      t.equal(safe1.status, 200);
-      t.equal(safe2.status, 200);
-      t.equal(unsafe1.status, 500);
-      t.equal(unsafe2.status, 500);
-      t.match(stdout, /Starting agent/);
-    })
-    .catch((error) => {
-      t.fail(error);
-    })
-    .finally(() => {
-      server.kill();
+t.test(
+  "it blocks in blocking mode",
+  {
+    skip: isGitHubActions ? "cannot bind to port 443 on github actions" : false,
+  },
+  (t) => {
+    const server = spawn(`node`, [pathToApp], {
+      env: {
+        ...process.env,
+        AIKIDO_DEBUG: "true",
+        AIKIDO_BLOCKING: "true",
+        // Required for self-signed certificates
+        // This is to allow the sample app to make insecure requests
+        NODE_TLS_REJECT_UNAUTHORIZED: "0",
+      },
     });
-});
+
+    // Required for self-signed certificates
+    // See fetch(...) calls below
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+    server.on("close", () => {
+      t.end();
+    });
+
+    server.on("error", (err) => {
+      t.fail(err);
+    });
+
+    let stdout = "";
+    server.stdout.on("data", (data) => {
+      stdout += data.toString();
+    });
+
+    let stderr = "";
+    server.stderr.on("data", (data) => {
+      stderr += data.toString();
+    });
+
+    // Wait for the server to start
+    timeout(2000)
+      .then(() => {
+        return Promise.all([
+          fetch(
+            `http://localhost/request?url=${encodeURIComponent("https://localhost")}`,
+            {
+              signal: AbortSignal.timeout(5000),
+            }
+          ),
+          fetch(
+            `https://localhost/request?url=${encodeURIComponent("http://localhost")}`,
+            {
+              signal: AbortSignal.timeout(5000),
+            }
+          ),
+          fetch(
+            `http://localhost/request?url=${encodeURIComponent("http://localhost:42313")}`,
+            {
+              signal: AbortSignal.timeout(5000),
+            }
+          ),
+          fetch(
+            `https://localhost/request?url=${encodeURIComponent("http://localhost:42313")}`,
+            {
+              signal: AbortSignal.timeout(5000),
+            }
+          ),
+        ]);
+      })
+      .then(([safe1, safe2, unsafe1, unsafe2]) => {
+        t.equal(safe1.status, 200);
+        t.equal(safe2.status, 200);
+        t.equal(unsafe1.status, 500);
+        t.equal(unsafe2.status, 500);
+        t.match(stdout, /Starting agent/);
+      })
+      .catch((error) => {
+        t.fail(error);
+      })
+      .finally(() => {
+        server.kill();
+      });
+  }
+);
