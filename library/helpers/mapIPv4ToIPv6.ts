4051df4eb780bfa93949e502a0599e2876266659
Timo Kössler <info@timokoessler.de>
2025-03-20 15:05:08 +0100

    Update library/helpers/mapIPv4ToIPv6.ts
    Co-authored-by: BitterPanda <bitterpanda@proton.me>

--- library/helpers/mapIPv4ToIPv6.ts
+++ library/helpers/mapIPv4ToIPv6.ts
@@ -10,5 +10,6 @@ export default function mapIPv4ToIPv6(ip: string): string {
 
   const parts = ip.split("/");
   const suffix = Number.parseInt(parts[1], 10);
+  // we add 96 to the suffix, since ::ffff: already is 96 bits, so the 32 remaining bits are decided by the IPv4 address
   return `::ffff:${parts[0]}/${suffix + 96}`;
 }
