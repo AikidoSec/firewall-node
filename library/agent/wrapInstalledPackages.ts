74a12d85363fc4b47cc94c62e203c5091d104b5b
Timo Kössler <info@timokoessler.de>
2025-03-26 16:49:12 +0100

    Detect if zen is imported too late

--- library/agent/wrapInstalledPackages.ts
+++ library/agent/wrapInstalledPackages.ts
@@ -1,3 +1,4 @@
+import { isAnyPkgAlreadyRequired } from "../helpers/isAnyPkgAlreadyRequired";
 import { applyHooks } from "./applyHooks";
 import { Hooks } from "./hooks/Hooks";
 import { Wrapper } from "./Wrapper";
@@ -7,6 +8,13 @@ export function wrapInstalledPackages(wrappers: Wrapper[]) {
   wrappers.forEach((wrapper) => {
     wrapper.wrap(hooks);
   });
+
+  if (isAnyPkgAlreadyRequired(hooks.getPackages())) {
+    // eslint-disable-next-line no-console
+    console.warn(
+      "Aikido: Some packages can't be protected because they were imported before Aikido was initialized. Please make sure to import Aikido as the first module in your application."
+    );
+  }
 
   return applyHooks(hooks);
 }
