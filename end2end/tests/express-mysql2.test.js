6a4577e10848a4e9e2cf883051674a61076d22fd
Timo Kössler <info@timokoessler.de>
2025-03-26 17:15:23 +0100

    Add e2e test

--- end2end/tests/express-mysql2.test.js
+++ end2end/tests/express-mysql2.test.js
@@ -114,6 +114,74 @@ t.test("it does not block in dry mode", (t) => {
       t.equal(normalSearch.status, 200);
       t.match(stdout, /Starting agent/);
       t.notMatch(stderr, /Zen has blocked an SQL injection/);
+      t.notMatch(
+        stderr,
+        /Some packages can't be protected because they were imported before Aikido was initialized\. Please make sure to import Aikido as the first module in your application\./
+      );
+    })
+    .catch((error) => {
+      t.fail(error.message);
+    })
+    .finally(() => {
+      server.kill();
+    });
+});
+
+t.test("it prints wrong import warning", (t) => {
+  const server = spawn(`node`, [pathToApp, "4002"], {
+    env: {
+      ...process.env,
+      AIKIDO_DEBUG: "true",
+      AIKIDO_BLOCK: "true",
+      TEST_IMPORT_TOO_LATE: "true",
+    },
+  });
+
+  server.on("close", () => {
+    t.end();
+  });
+
+  let stdout = "";
+  server.stdout.on("data", (data) => {
+    stdout += data.toString();
+  });
+
+  let stderr = "";
+  server.stderr.on("data", (data) => {
+    stderr += data.toString();
+  });
+
+  // Wait for the server to start
+  timeout(2000)
+    .then(() =>
+      Promise.all([
+        fetch(
+          `http://localhost:4002/?petname=${encodeURIComponent("Njuska'); DELETE FROM cats;-- H")}`,
+          {
+            signal: AbortSignal.timeout(5000),
+          }
+        ),
+        fetch(
+          `http://localhost:4002/cats/${encodeURIComponent("Njuska'; DELETE FROM cats;-- H")}`,
+          {
+            signal: AbortSignal.timeout(5000),
+          }
+        ),
+        fetch("http://localhost:4002/?petname=Njuska", {
+          signal: AbortSignal.timeout(5000),
+        }),
+      ])
+    )
+    .then(([sqlInjection, sqlInjection2, normalSearch]) => {
+      t.equal(sqlInjection.status, 200);
+      t.equal(sqlInjection2.status, 200);
+      t.equal(normalSearch.status, 200);
+      t.match(stdout, /Starting agent/);
+      t.match(
+        stderr,
+        /Some packages can't be protected because they were imported before Aikido was initialized\. Please make sure to import Aikido as the first module in your application\./
+      );
+      t.notMatch(stderr, /Zen has blocked an SQL injection/);
     })
     .catch((error) => {
       t.fail(error.message);
