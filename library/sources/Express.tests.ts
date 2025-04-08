25a0b5b3288e7bd90820d3dab6715d6e76d3ff07
Timo Kössler <info@timokoessler.de>
2025-04-08 13:49:54 +0200

    Fix unit tests

--- library/sources/Express.tests.ts
+++ library/sources/Express.tests.ts
@@ -394,7 +394,7 @@ export function createExpressTests(expressPackageName: string) {
     t.same(response.statusCode, 500);
     t.match(agent.getInspectionStatistics().getStats(), {
       requests: {
-        total: 1,
+        total: 0,
         attacksDetected: {
           total: 1,
           blocked: 1,
