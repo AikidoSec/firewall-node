fb9e683659e61f299903a613c96d78c2071de27d
Timo Kössler <info@timokoessler.de>
2025-04-16 14:02:40 +0200

    Extend comment

--- library/vulnerabilities/path-traversal/containsUnsafePathParts.ts
+++ library/vulnerabilities/path-traversal/containsUnsafePathParts.ts
@@ -12,6 +12,11 @@ export function containsUnsafePathParts(filePath: string) {
 
 /**
  * This function is used for urls, because they can contain a TAB, carriage return or line feed that is silently removed by the URL constructor.
+ *
+ * The WHATWG URL spec defines the following:
+ * - Remove all ASCII tab or newline from input.
+ * - An ASCII tab or newline is U+0009 TAB, U+000A LF, or U+000D CR.
+ *
  * See https://url.spec.whatwg.org/#url-parsing
  */
 export function containsUnsafePathPartsUrl(filePath: string) {
