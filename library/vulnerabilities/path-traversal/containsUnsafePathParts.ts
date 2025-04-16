809ebe6f08e4a7a3b0ffa87e430689eaf1f89745
Timo Kössler <info@timokoessler.de>
2025-04-16 13:02:27 +0200

    Fix url path traversal bypass

--- library/vulnerabilities/path-traversal/containsUnsafePathParts.ts
+++ library/vulnerabilities/path-traversal/containsUnsafePathParts.ts
@@ -9,3 +9,11 @@ export function containsUnsafePathParts(filePath: string) {
 
   return false;
 }
+
+/**
+ * This function is used for urls, because they can contain a TAB, carriage return or line feed that is silently removed by the URL constructor.
+ * See https://url.spec.whatwg.org/#url-parsing
+ */
+export function containsUnsafePathPartsUrl(filePath: string) {
+  return /(?:\.(?:\t|\n|\r)?){2}(?:\/|\\)/.test(filePath);
+}
