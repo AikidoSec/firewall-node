3be4071b9e2250e1dd8ca8793250148a7d496fda
Timo Kössler <info@timokoessler.de>
2025-05-07 09:56:13 +0200

    Remove --experimental-sqlite

--- library/agent/hooks/isBuiltinModule.test.ts
+++ library/agent/hooks/isBuiltinModule.test.ts
@@ -1,5 +1,6 @@
 import * as t from "tap";
 import { isBuiltinModule } from "./isBuiltinModule";
+import { getMajorNodeVersion } from "../../helpers/getNodeVersion";
 
 t.test("it works", async (t) => {
   t.equal(isBuiltinModule("fs"), true);
@@ -8,4 +9,7 @@ t.test("it works", async (t) => {
   t.equal(isBuiltinModule("node:http"), true);
   t.equal(isBuiltinModule("test"), false);
   t.equal(isBuiltinModule(""), false);
+  if (getMajorNodeVersion() >= 24) {
+    t.equal(isBuiltinModule("node:sqlite"), true);
+  }
 });
