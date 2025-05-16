58a523d8eb58ec09751f81c2941b6b282753821a
Hans Ott <hansott@hotmail.be>
2025-05-16 17:38:40 +0200

    Use `Map` instead of object

--- library/helpers/formDataToPlainObject.ts
+++ library/helpers/formDataToPlainObject.ts
@@ -1,23 +1,27 @@
-import { isPlainObject } from "./isPlainObject";
-
 export function formDataToPlainObject(formData: FormData) {
-  const object: Record<string, unknown> = {};
+  const object: Map<string, unknown> = new Map();
   formData.forEach((value, key) => {
-    if (typeof value === "object" && !isPlainObject(value)) {
+    if (typeof value !== "string") {
       return;
     }
 
-    if (!(key in object)) {
-      object[key] = value;
+    if (object.has(key)) {
+      // If the key already exists, treat it as an array
+      const entry = object.get(key);
+
+      if (Array.isArray(entry)) {
+        // If it's already an array, just push the new value
+        entry.push(value);
+        return;
+      }
+
+      // Convert it to an array
+      object.set(key, [object.get(key), value]);
       return;
     }
 
-    if (!Array.isArray(object[key])) {
-      object[key] = [object[key], value];
-      return;
-    }
-
-    object[key].push(value);
+    object.set(key, value);
   });
-  return object;
+
+  return Object.fromEntries(object);
 }
