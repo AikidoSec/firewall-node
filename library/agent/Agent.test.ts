cd4e4445a5e9cf66579cb2e81aa4f26c10db84e5
Hans Ott <hansott@hotmail.be>
2025-05-30 16:14:54 +0200

    Add failing test to see that packages are not cleared after heartbeat

--- library/agent/Agent.test.ts
+++ library/agent/Agent.test.ts
@@ -1170,3 +1170,66 @@ t.test("it only allows some IP addresses", async () => {
     allowed: true,
   });
 });
+
+t.test("packages are not cleared after heartbeat", async () => {
+  const clock = FakeTimers.install();
+
+  const logger = new LoggerNoop();
+  const api = new ReportingAPIForTesting();
+  const agent = createTestAgent({
+    api,
+    logger,
+    token: new Token("123"),
+    suppressConsoleLog: false,
+  });
+  agent.start([]);
+
+  agent.onPackageRequired("express", "4.17.1");
+
+  api.clear();
+
+  await agent.flushStats(1000);
+
+  t.match(api.getEvents(), [
+    {
+      type: "heartbeat",
+      packages: [
+        {
+          name: "@aikidosec/firewall",
+          version: "0.0.0",
+          requiredAt: 0,
+        },
+        {
+          name: "express",
+          version: "4.17.1",
+          requiredAt: 0,
+        },
+      ],
+    },
+  ]);
+
+  api.clear();
+
+  // Send second heartbeat - packages should still be there
+  await agent.flushStats(1000);
+
+  t.match(api.getEvents(), [
+    {
+      type: "heartbeat",
+      packages: [
+        {
+          name: "@aikidosec/firewall",
+          version: "0.0.0",
+          requiredAt: 0,
+        },
+        {
+          name: "express",
+          version: "4.17.1",
+          requiredAt: 0,
+        },
+      ],
+    },
+  ]);
+
+  clock.uninstall();
+});
