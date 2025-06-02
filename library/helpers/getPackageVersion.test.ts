7b37402cab69805294348bb488d3080e3c7e2bab
Timo Kössler <info@timokoessler.de>
2025-06-02 14:09:34 +0200

    Fix unit tests

--- library/helpers/getPackageVersion.test.ts
+++ library/helpers/getPackageVersion.test.ts
@@ -4,5 +4,5 @@ import { getPackageVersion } from "./getPackageVersion";
 t.test("it resolves the version of a package", async (t) => {
   t.same(getPackageVersion("express"), "5.1.0");
   t.same(getPackageVersion("non-existing-package"), null);
-  t.same(getPackageVersion("@google-cloud/functions-framework"), "3.5.1");
+  t.same(getPackageVersion("@google-cloud/functions-framework"), "4.0.0");
 });
