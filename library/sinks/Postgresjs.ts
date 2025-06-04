2804dda650ba64b1e44a4cd383c59dfdf57b5e2a
Hans Ott <hansott@hotmail.be>
2025-06-04 11:15:28 +0200

    Add missing module

--- library/sinks/Postgresjs.ts
+++ library/sinks/Postgresjs.ts
@@ -25,6 +25,7 @@ export class Postgresjs implements Wrapper {
         context: context,
         operation: "sql.unsafe",
         dialect: this.dialect,
+        module: "postgres",
       });
     }
 
