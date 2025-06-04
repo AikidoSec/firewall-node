dd62fb9b5eb9ba542ff3a33942623a43a1d0e9d4
Hans Ott <hansott@hotmail.be>
2025-06-04 13:36:50 +0200

    Remove unused eslint-disable comments
    /home/runner/work/firewall-node/firewall-node/library/sinks/ChildProcess.ts
      1:1  warning  Unused eslint-disable directive (no problems were reported from 'max-lines-per-function')
    
    /home/runner/work/firewall-node/firewall-node/library/sinks/FileSystem.ts
      1:1  warning  Unused eslint-disable directive (no problems were reported from 'max-lines-per-function')
    
    /home/runner/work/firewall-node/firewall-node/library/sources/Fastify.ts
      1:1  warning  Unused eslint-disable directive (no problems were reported from 'max-lines-per-function')
    
    /home/runner/work/firewall-node/firewall-node/library/sources/HTTPServer.ts
      1:1  warning  Unused eslint-disable directive (no problems were reported from 'max-lines-per-function')
    
    ✖ 4 problems (0 errors, 4 warnings)
      0 errors and 4 warnings potentially fixable with the `--fix` option.

--- library/sinks/ChildProcess.ts
+++ library/sinks/ChildProcess.ts
@@ -1,4 +1,3 @@
-/* eslint-disable max-lines-per-function */
 import { getContext } from "../agent/Context";
 import { Hooks } from "../agent/hooks/Hooks";
 import { InterceptorResult } from "../agent/hooks/InterceptorResult";
