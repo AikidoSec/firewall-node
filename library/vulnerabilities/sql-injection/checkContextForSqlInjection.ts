73ded4c900f345e81ce5da5106fb02e1bade3dd9
Hans Ott <hansott@hotmail.be>
2025-06-04 11:44:15 +0200

    Fix linting

--- library/vulnerabilities/sql-injection/checkContextForSqlInjection.ts
+++ library/vulnerabilities/sql-injection/checkContextForSqlInjection.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 import { getInstance } from "../../agent/AgentSingleton";
 import { Context } from "../../agent/Context";
 import { InterceptorResult } from "../../agent/hooks/InterceptorResult";
