5e8f7b570c91105a6e4c7505b04bdb4b676f7804
Hans Ott <hansott@hotmail.be>
2025-06-04 13:42:10 +0200

    Revert "Fix test"
    This reverts commit d84f5a3090c58af7fe2d891555104b72898efb68.

--- library/sources/Lambda.test.ts
+++ library/sources/Lambda.test.ts
@@ -315,7 +315,6 @@ t.test("it sends heartbeat after first and every 10 minutes", async () => {
         ipAddresses: {
           breakdown: {},
         },
-        sqlTokenizationFailures: 0,
       },
       middlewareInstalled: false,
     },
