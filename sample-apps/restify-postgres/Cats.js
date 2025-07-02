e4930421e7dda82de3576adf9d6d75aa4d8cdec9
Hans Ott <hansott@hotmail.be>
2025-07-02 11:45:56 +0200

    Add sample app for node-restify

---
+++ sample-apps/restify-postgres/Cats.js
@@ -0,0 +1,18 @@
+class Cats {
+  constructor(db) {
+    this.db = db;
+  }
+
+  async add(name) {
+    // This is unsafe! This is for demo purposes only, you should use parameterized queries.
+    await this.db.query(`INSERT INTO cats_2 (petname) VALUES ('${name}');`);
+  }
+
+  async getAll() {
+    const cats = await this.db.query("SELECT petname FROM cats_2;");
+
+    return cats.rows.map((row) => row.petname);
+  }
+}
+
+module.exports = Cats; 
