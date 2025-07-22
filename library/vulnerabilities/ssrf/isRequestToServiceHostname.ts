56d2cf0af9e4cec769c8e8794074d902670c7e3c
Hans Ott <hansott@hotmail.be>
2025-07-22 18:46:43 +0200

    Add function to determine if hostname is service hostname

---
+++ library/vulnerabilities/ssrf/isRequestToServiceHostname.ts
@@ -0,0 +1,22 @@
+// e.g. "discord-bot" or "my_service" or "backend"
+const SERVICE_HOSTNAME_PATTERN = /^[a-z-_]+$/;
+
+const SPECIAL_EXCEPTIONS = [
+  "localhost",
+  "localdomain",
+
+  // On GCP "metadata" resolves to the IMDS service
+  // See https://stackoverflow.com/questions/23362887/can-you-get-external-ip-address-from-within-a-google-compute-vm-instance
+  // See https://cloud.google.com/compute/docs/internal-dns
+  "metadata",
+];
+
+export function isRequestToServiceHostname(hostname: string): boolean {
+  const lowerHostname = hostname.toLowerCase();
+
+  if (SPECIAL_EXCEPTIONS.includes(lowerHostname)) {
+    return false;
+  }
+
+  return SERVICE_HOSTNAME_PATTERN.test(lowerHostname);
+}
