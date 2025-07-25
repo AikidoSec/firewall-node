7cc3df31b555308af1edbbb18e76066b9b83c896
Hans Ott <hansott@hotmail.be>
2025-07-25 15:52:32 +0200

    Add hostname

--- library/vulnerabilities/ssrf/isRequestToServiceHostname.ts
+++ library/vulnerabilities/ssrf/isRequestToServiceHostname.ts
@@ -5,7 +5,7 @@ const NOT_SERVICE_HOSTNAMES = [
   "localhost",
   "localdomain",
 
-  // On GCP "metadata" resolves to the IMDS service
+  // On GCP "metadata" resolves to the IMDS service (metadata.google.internal)
   // See https://stackoverflow.com/questions/23362887/can-you-get-external-ip-address-from-within-a-google-compute-vm-instance
   // See https://cloud.google.com/compute/docs/internal-dns
   "metadata",
