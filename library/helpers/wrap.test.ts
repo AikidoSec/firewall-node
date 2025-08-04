2f1d8b3f99ec5bae0367a5c2d80d3f1b4b67c884
Timo Kössler <info@timokoessler.de>
2025-08-04 11:05:19 +0200

    Fix double wrapping HTTP server
    + Add some wrapping tests

--- library/helpers/wrap.test.ts
+++ library/helpers/wrap.test.ts
@@ -55,3 +55,71 @@ t.test("it returns false for unwrapped function or property", async (t) => {
   const test = () => "test";
   t.same(isWrapped(test), false);
 });
+
+t.test("it preserves the original function's properties", async (t) => {
+  const originalFunction = function original() {};
+  originalFunction.someProperty = "someValue";
+
+  const moduleObj = { myMethod: originalFunction };
+  Object.defineProperty(moduleObj, "getterAndSetter", {
+    get: () => () => "42",
+    set: () => {
+      return 0;
+    },
+    configurable: true,
+    enumerable: true,
+  });
+
+  Object.defineProperty(moduleObj, "nonEnumerable", {
+    configurable: true,
+    enumerable: false,
+    value: () => "nonEnumerable",
+  });
+
+  wrap(moduleObj, "myMethod", (original) => {
+    return function wrapped() {
+      return original();
+    };
+  });
+
+  wrap(moduleObj, "getterAndSetter", (original) => {
+    return function wrapped() {
+      return "Zen";
+    };
+  });
+
+  wrap(moduleObj, "nonEnumerable", (original) => {
+    return function wrapped() {
+      return "Zen";
+    };
+  });
+
+  t.same(isWrapped(moduleObj.myMethod), true);
+  t.same(moduleObj.myMethod.someProperty, "someValue");
+  // @ts-expect-error Defined by Zen
+  t.same(moduleObj.myMethod.__original, originalFunction);
+  // @ts-expect-error Defined by Zen
+  t.same(moduleObj.myMethod.__wrapped, true);
+
+  // @ts-expect-error Not auto detected by TypeScript
+  t.same(moduleObj.getterAndSetter(), "Zen");
+  // @ts-expect-error Not auto detected by TypeScript
+  t.same(moduleObj.getterAndSetter.__original(), "42");
+  // @ts-expect-error Not auto detected by TypeScript
+  t.same(moduleObj.getterAndSetter.__wrapped, true);
+  t.same(
+    Object.getOwnPropertyDescriptor(moduleObj, "getterAndSetter")!.enumerable,
+    true
+  );
+
+  // @ts-expect-error Not auto detected by TypeScript
+  t.same(moduleObj.nonEnumerable(), "Zen");
+  // @ts-expect-error Not auto detected by TypeScript
+  t.same(moduleObj.nonEnumerable.__original(), "nonEnumerable");
+  // @ts-expect-error Not auto detected by TypeScript
+  t.same(moduleObj.nonEnumerable.__wrapped, true);
+  t.same(
+    Object.getOwnPropertyDescriptor(moduleObj, "nonEnumerable")!.enumerable,
+    false
+  );
+});
