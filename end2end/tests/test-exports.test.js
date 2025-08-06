dc400498da536ec5df643d259243da1f8289084c
Hans Ott <hansott@hotmail.be>
2025-08-06 19:08:47 +0200

    Remove stdio inherit

--- end2end/tests/test-exports.test.js
+++ end2end/tests/test-exports.test.js
@@ -9,7 +9,6 @@ t.test("all package.json exports should work with CommonJS", async (t) => {
   try {
     execSync("node test.js", {
       cwd: pathToApp,
-      stdio: "inherit",
     });
   } catch (error) {
     t.fail("CommonJS exports test failed");
@@ -23,12 +22,10 @@ t.test(
     try {
       execSync("node_modules/.bin/tsc -p tsconfig.node18.json", {
         cwd: pathToApp,
-        stdio: "inherit",
       });
 
       execSync("node dist18/test.js", {
         cwd: pathToApp,
-        stdio: "inherit",
       });
     } catch (error) {
       t.fail("TypeScript Node 18 exports test failed");
@@ -43,12 +40,10 @@ t.test(
     try {
       execSync("node_modules/.bin/tsc -p tsconfig.node24.json", {
         cwd: pathToApp,
-        stdio: "inherit",
       });
 
       execSync("node dist24/test.js", {
         cwd: pathToApp,
-        stdio: "inherit",
       });
     } catch (error) {
       t.fail("TypeScript Node 24 exports test failed");
@@ -63,7 +58,6 @@ t.test(
     try {
       execSync("node --experimental-strip-types test.ts", {
         cwd: pathToApp,
-        stdio: "inherit",
       });
     } catch (error) {
       t.fail("Node 24 strip-types exports test failed");
