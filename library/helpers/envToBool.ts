ff536fdf3b2bfe1fc3228da495003fc6eb2503aa
Hans Ott <hansott@hotmail.be>
2025-08-07 14:38:57 +0200

    Revert rename

--- library/helpers/envToBool.ts
+++ library/helpers/envToBool.ts
@@ -3,9 +3,9 @@ const trueValues = ["true", "1", "yes", "y", "on"];
 /**
  * Parses the string value of an environment variable to a boolean.
  */
-export function envToBool(env: string | undefined): boolean {
-  if (!env) {
+export function envToBool(envName: string | undefined): boolean {
+  if (!envName) {
     return false;
   }
-  return trueValues.includes(env.toLowerCase());
+  return trueValues.includes(envName.toLowerCase());
 }
