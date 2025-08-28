0d2db36da630d8fe5cc3b9e0160f94ddc38528b6
Timo Kössler <info@timokoessler.de>
2025-08-28 18:31:39 +0200

    Change sql approach to be more performant
    but detect less

--- library/vulnerabilities/attack-wave-detection/containsSQLSyntax.ts
+++ library/vulnerabilities/attack-wave-detection/containsSQLSyntax.ts
@@ -1,61 +1,20 @@
 import type { Context } from "../../agent/Context";
 import { extractStringsFromUserInputCached } from "../../helpers/extractStringsFromUserInputCached";
 
-// Match spaces, tabs inline comments like /* test */ and +
-const sqlSpace = "(\\s|\\/\\*.{1,50}\\*\\/|\\+)";
-
-const forbiddenKeywords = [
-  "@@innodb_version",
-  "xp_cmdshell",
-  "innodb_table_stats",
-  "pg_client_encoding",
-  "pg_shadow",
-  "pg_group",
-  "sleep\\(.{1,50}\\)",
-].join("|");
-
-const builtinDbTables = [
-  "mysql\\.\\w{4,53}",
-  "information_schema\\.\\w{4,38}",
-].join("|");
-
-const queries = [
-  `select${sqlSpace}\\*${sqlSpace}from`,
-  `union${sqlSpace}(all${sqlSpace})?select`,
-  `insert${sqlSpace}into${sqlSpace}\\w+${sqlSpace}\\(`,
-  `create${sqlSpace}user${sqlSpace}\\w+${sqlSpace}identified${sqlSpace}by`,
-  `backup${sqlSpace}database${sqlSpace}\\w+${sqlSpace}to`,
-  `update${sqlSpace}\\w+${sqlSpace}set${sqlSpace}\\w+${sqlSpace}?=`,
-  `drop${sqlSpace}table${sqlSpace}\\w+`,
-  `truncate${sqlSpace}table${sqlSpace}\\w+`,
-  `delete${sqlSpace}from${sqlSpace}\\w+`,
-].join("|");
-
-const queryParts = [
-  `(or|and|where|having|&{2}|\\|{2})${sqlSpace}\\w+${sqlSpace}?(=|<)\\w`,
-  `("|')${sqlSpace}?(or|and|where|having|&{2}|\\|{2}|<|=)${sqlSpace}?("|')`,
-].join("|");
-
-const sqlPathRegex = new RegExp(
-  `(${forbiddenKeywords}|${queries}|${builtinDbTables}|${queryParts})`,
-  "i"
-);
+const keywords = [
+  "SELECT (CASE WHEN",
+  "SELECT COUNT(",
+  "SLEEP(",
+  "WAITFOR DELAY",
+  "SELECT LIKE(CHAR(",
+  "INFORMATION_SCHEMA.COLUMNS",
+  ",MD5(",
+  "DBMS_PIPE.RECEIVE_MESSAGE",
+  "SYSIBM.SYSTABLES",
+  "RANDOMBLOB(",
+];
 
 export function containsSQLSyntax(context: Context): boolean {
-  if (queryContainsSQLSyntax(context)) {
-    return true;
-  }
-
-  if (context.route && context.route.length > 5) {
-    if (sqlPathRegex.test(context.route)) {
-      return true;
-    }
-  }
-
-  return false;
-}
-
-function queryContainsSQLSyntax(context: Context): boolean {
   const queryStrings = extractStringsFromUserInputCached(context, "query");
   if (!queryStrings) {
     return false;
@@ -66,8 +25,11 @@ function queryContainsSQLSyntax(context: Context): boolean {
       continue;
     }
 
-    if (sqlPathRegex.test(str)) {
-      return true;
+    const upperStr = str.toUpperCase();
+    for (const keyword of keywords) {
+      if (upperStr.includes(keyword)) {
+        return true;
+      }
     }
   }
   return false;
