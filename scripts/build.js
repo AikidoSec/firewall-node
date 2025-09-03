f4909121cd5b4bf451ad72a0142e6983c458aead
Timo Kössler <info@timokoessler.de>
2025-09-03 09:29:29 +0200

    Fix Zen internals version not cached

--- scripts/build.js
+++ scripts/build.js
@@ -66,6 +66,7 @@ async function dlZenInternals() {
     console.log("Zen Internals already installed. Skipping download.");
     return;
   }
+  console.log("Downloading Zen Internals...");
 
   await downloadFile(
     `${INTERNALS_URL}/${tarballFile}`,
@@ -81,6 +82,8 @@ async function dlZenInternals() {
   await rm(join(internalsDir, tarballFile));
   await rm(join(internalsDir, checksumFile));
   await rm(join(internalsDir, "zen_internals.d.ts"));
+
+  await writeFile(versionCacheFile, INTERNALS_VERSION);
 }
 
 async function modifyDtsFilesAfterBuild() {
