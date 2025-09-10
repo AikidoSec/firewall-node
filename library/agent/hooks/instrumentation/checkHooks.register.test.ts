24d6403079fbdbcd880b27852866ea03dfca5e23
Timo Kössler <info@timokoessler.de>
2025-09-10 15:49:05 +0200

    Only run test on recent Node.js versions

--- library/agent/hooks/instrumentation/checkHooks.register.test.ts
+++ library/agent/hooks/instrumentation/checkHooks.register.test.ts
@@ -1,6 +1,7 @@
 import * as t from "tap";
 import { wrap } from "../../../helpers/wrap";
 import { registerNodeHooks } from ".";
+import * as mod from "node:module";
 
 const logs: string[] = [];
 wrap(console, "warn", function warn() {
@@ -9,12 +10,18 @@ wrap(console, "warn", function warn() {
   };
 });
 
-t.test("it works", async (t) => {
-  // This function already calls checkHooks, so we don't need to call it again
-  registerNodeHooks();
+t.test(
+  "it works",
+  {
+    skip: !("registerHooks" in mod) ? "Recent Node.js version required" : false,
+  },
+  async (t) => {
+    // This function already calls checkHooks, so we don't need to call it again
+    registerNodeHooks();
 
-  // Required because the check itself is async
-  await new Promise((resolve) => setTimeout(resolve, 100));
+    // Required because the check itself is async
+    await new Promise((resolve) => setTimeout(resolve, 100));
 
-  t.same(logs, []);
-});
+    t.same(logs, []);
+  }
+);
