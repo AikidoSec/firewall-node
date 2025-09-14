729043d5faa8cb68c62fae163d0161bc8a4a197a
Timo Kössler <info@timokoessler.de>
2025-09-14 13:06:43 +0200

    Add deno check again

--- library/helpers/isFirewallSupported.ts
+++ library/helpers/isFirewallSupported.ts
@@ -2,6 +2,16 @@
 import { getMajorNodeVersion } from "./getNodeVersion";
 
 export default function isFirewallSupported() {
+  // @ts-expect-error Unknown type of globalThis
+  if (globalThis.Deno) {
+    const runtimeName = "Deno";
+    console.error(
+      `Error: Aikido Firewall does not support ${runtimeName}. If you want support for ${runtimeName}, please contact us: hello@aikido.dev`
+    );
+
+    return false;
+  }
+
   // Check for unsupported Node.js versions
   if (getMajorNodeVersion() < 16) {
     console.error(
