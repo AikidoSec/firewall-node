3ff8fffb410a7bf9e9133ff82d1ec530bd4fc71a
Hans Ott <hansott@hotmail.be>
2025-09-25 15:51:37 +0200

    Remove AI SDK exclusions for run-tap.js

--- scripts/run-tap.js
+++ scripts/run-tap.js
@@ -28,10 +28,7 @@ if (process.argv.includes("--test-new-instrumentation")) {
   const excludedTestFilesForNewInstrumentation = [
     "**/sinks/Prisma.test.ts",
     "**/sinks/AwsSDK*",
-    "**/sinks/AiSDK.test.ts",
     "**/sinks/OpenAI.test.ts",
-    "**/sinks/AiSDK.v4.test.ts",
-    "**/sinks/AiSDK.v5.test.ts",
 
     "**/sources/Lambda.test.ts",
     "**/sources/FunctionsFramework.test.ts",
