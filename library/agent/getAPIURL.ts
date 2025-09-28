99da13e1837d53d5d33625b9de726ee746f07691
Hans Ott <hansott@hotmail.be>
2025-09-28 21:02:44 +0200

    Add support for multi environment tokens (EU/US/ME)

--- library/agent/getAPIURL.ts
+++ library/agent/getAPIURL.ts
@@ -1,6 +1,17 @@
+import { extractRegionFromToken } from "./extractRegionFromToken";
+
 export function getAPIURL() {
   if (process.env.AIKIDO_ENDPOINT) {
     return new URL(process.env.AIKIDO_ENDPOINT);
+  }
+
+  const region = extractRegionFromToken(process.env.AIKIDO_TOKEN || '');
+
+  if (region === 'US') {
+    return new URL("https://guard.us.aikido.dev");
+  }
+  if (region === 'ME') {
+    return new URL("https://guard.me.aikido.dev");
   }
 
   return new URL("https://guard.aikido.dev");
