e883b32421b1a3d0bc3543cf2299ab76a4e039ba
Hans Ott <hansott@hotmail.be>
2025-10-01 15:15:49 +0200

    Extract region from token for realtime polling service

--- library/agent/realtime/getRealtimeURL.ts
+++ library/agent/realtime/getRealtimeURL.ts
@@ -1,6 +1,18 @@
+import { extractRegionFromToken } from "../extractRegionFromToken";
+
 export function getRealtimeURL() {
   if (process.env.AIKIDO_REALTIME_ENDPOINT) {
     return new URL(process.env.AIKIDO_REALTIME_ENDPOINT);
+  }
+
+  const region = extractRegionFromToken(process.env.AIKIDO_TOKEN || "");
+
+  if (region === "US") {
+    return new URL("https://runtime.us.aikido.dev");
+  }
+
+  if (region === "ME") {
+    return new URL("https://runtime.me.aikido.dev");
   }
 
   return new URL("https://runtime.aikido.dev");
