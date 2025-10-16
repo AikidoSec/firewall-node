c639a9a3c66802e8edfdb61cc600dd1b07b48e06
Timo Kössler <info@timokoessler.de>
2025-10-16 13:01:09 +0200

    Add adonis sample app with sqlite

---
+++ sample-apps/adonis-sqlite/start/routes.ts
@@ -0,0 +1,16 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router'
+
+router.get('/', async () => {
+  return {
+    hello: 'world',
+  }
+})
