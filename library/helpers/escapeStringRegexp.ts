33561b4e5b3e2caa4486e0dd0f966583a762c983
Timo Kössler <info@timokoessler.de>
2025-11-27 13:55:00 +0100

    Use built-in RegExp.escape if available

--- library/helpers/escapeStringRegexp.ts
+++ library/helpers/escapeStringRegexp.ts
@@ -6,5 +6,13 @@
  * Taken from https://github.com/sindresorhus/escape-string-regexp/
  */
 export function escapeStringRegexp(string: string) {
+  // Use native implementation if available (Node.js 24+)
+  // A benchmark showed that it is up to 50% faster than the polyfill
+  // @ts-expect-error Outdated Node.js types
+  if (typeof RegExp?.escape === "function") {
+    // @ts-expect-error Outdated Node.js types
+    return RegExp.escape(string);
+  }
+
   return string.replace(/[|\\{}()[\]^$+*?.]/g, "\\$&").replace(/-/g, "\\x2d");
 }
