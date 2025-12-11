902cb2778b1f43f7ec47e0d91574b8a1d4be1bbb
Timo Kössler <info@timokoessler.de>
2025-12-11 09:27:31 +0100

    Apply review suggestion

--- library/helpers/escapeStringRegexp.ts
+++ library/helpers/escapeStringRegexp.ts
@@ -1,3 +1,5 @@
+let funcToExport: (string: string) => string;
+
 /**
  * Escape characters with special meaning either inside or outside character sets.
  *
@@ -5,14 +7,18 @@
  *
  * Taken from https://github.com/sindresorhus/escape-string-regexp/
  */
-export function escapeStringRegexp(string: string) {
-  // Use native implementation if available (Node.js 24+)
-  // A benchmark showed that it is up to 50% faster than the polyfill
-  // @ts-expect-error Outdated Node.js types
-  if (typeof RegExp?.escape === "function") {
-    // @ts-expect-error Outdated Node.js types
-    return RegExp.escape(string);
-  }
-
+function escapeStringRegexpFallback(string: string) {
   return string.replace(/[|\\{}()[\]^$+*?.]/g, "\\$&").replace(/-/g, "\\x2d");
 }
+
+// Use native implementation if available (Node.js 24+)
+// A benchmark showed that it is up to 50% faster than the polyfill
+// @ts-expect-error Outdated Node.js types
+if (typeof RegExp?.escape === "function") {
+  // @ts-expect-error Outdated Node.js types
+  funcToExport = RegExp.escape;
+} else {
+  funcToExport = escapeStringRegexpFallback;
+}
+
+export { funcToExport as escapeStringRegexp };
