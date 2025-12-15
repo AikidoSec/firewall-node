81aa5e30bcf10d5a53846afa6e63dc24347d1588
Timo Kössler <info@timokoessler.de>
2025-12-15 10:14:03 +0100

    Fix benchmarks

--- library/helpers/isLibBundled.ts
+++ library/helpers/isLibBundled.ts
@@ -3,7 +3,8 @@ import { sep } from "path";
 // Detect at runtime if the library is bundled inside an application
 export function isLibBundled(): boolean {
   return (
-    !__dirname.includes(`node_modules${sep}@aikidosec${sep}firewall`) &&
-    !__dirname.includes(`firewall-node${sep}build`) // In case of e2e tests
+    !__filename.includes(
+      `node_modules${sep}@aikidosec${sep}firewall${sep}isLibBundled`
+    ) && !__filename.includes(`${sep}build${sep}isLibBundled`) // In case of e2e tests
   );
 }
