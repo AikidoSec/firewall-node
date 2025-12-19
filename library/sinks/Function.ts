ba373cfec625949f778e873c7c28cbe114ce3619
Hans Ott <hansott@hotmail.be>
2025-12-19 11:10:43 +0100

    Enable Function sink again, with support for eval
    By using a native addon that hooks into V8

--- library/sinks/Function.ts
+++ library/sinks/Function.ts
@@ -1,42 +1,104 @@
+import { join } from "node:path";
+import { getInstance } from "../agent/AgentSingleton";
 import { getContext } from "../agent/Context";
 import { Hooks } from "../agent/hooks/Hooks";
+import { InterceptorResult } from "../agent/hooks/InterceptorResult";
+import { onInspectionInterceptorResult } from "../agent/hooks/onInspectionInterceptorResult";
 import { Wrapper } from "../agent/Wrapper";
+import { getMajorNodeVersion } from "../helpers/getNodeVersion";
 import { checkContextForJsInjection } from "../vulnerabilities/js-injection/checkContextForJsInjection";
+import { existsSync } from "node:fs";
 
 export class Function implements Wrapper {
-  private inspectFunction(args: any[]) {
+  private inspectFunction(code: string): InterceptorResult {
     const context = getContext();
 
-    if (!context || !Array.isArray(args) || args.length === 0) {
+    if (!context) {
       return undefined;
     }
 
-    const findLastStringArg = (args: any[]) => {
-      for (let i = args.length - 1; i >= 0; --i) {
-        if (typeof args[i] === "string") {
-          return args[i];
-        }
-      }
-      return undefined;
-    };
-
-    const lastStringArg = findLastStringArg(args);
-
-    if (lastStringArg) {
-      return checkContextForJsInjection({
-        js: lastStringArg,
-        operation: "new Function",
-        context,
-      });
-    }
-
-    return undefined;
+    return checkContextForJsInjection({
+      js: code,
+      operation: "new Function/eval",
+      context,
+    });
   }
 
-  wrap(hooks: Hooks) {
-    hooks.addGlobal("Function", {
-      kind: "eval_op",
-      inspectArgs: this.inspectFunction,
+  wrap(_: Hooks) {
+    const majorVersion = getMajorNodeVersion();
+    const arch = process.arch;
+    const platform = process.platform;
+
+    const nodeInternalsDir = join(__dirname, "..", "node_internals");
+    const binaryPath = join(
+      nodeInternalsDir,
+      `zen-internals-node-${platform}-${arch}-node${majorVersion}.node`
+    );
+    if (!existsSync(binaryPath)) {
+      // oxlint-disable-next-line no-console
+      console.warn(
+        `Cannot find native addon for Node.js ${majorVersion} on ${platform}-${arch}. Function sink will not be instrumented.`
+      );
+      return;
+    }
+
+    const bindings: {
+      setCodeGenerationCallback: (
+        callback: (code: string) => string | undefined
+      ) => void;
+    } = require(binaryPath);
+    if (!bindings || typeof bindings.setCodeGenerationCallback !== "function") {
+      // oxlint-disable-next-line no-console
+      console.warn(
+        `Native addon for Node.js ${majorVersion} on ${platform}-${arch} is invalid. Function sink will not be instrumented.`
+      );
+      return;
+    }
+
+    bindings.setCodeGenerationCallback((code: string) => {
+      const agent = getInstance();
+      if (!agent) {
+        return;
+      }
+
+      const context = getContext();
+      if (context) {
+        const matches = agent.getConfig().getEndpoints(context);
+
+        if (matches.find((match) => match.forceProtectionOff)) {
+          return;
+        }
+      }
+
+      let inspectionResult: InterceptorResult | undefined;
+      const start = performance.now();
+      try {
+        inspectionResult = this.inspectFunction(code);
+      } catch (error: any) {
+        agent.onErrorThrownByInterceptor({
+          error: error,
+          method: "<compile>",
+          module: "Function/eval",
+        });
+      }
+
+      if (inspectionResult) {
+        try {
+          onInspectionInterceptorResult(
+            context,
+            agent,
+            inspectionResult,
+            { name: "Function/eval", type: "global" },
+            start,
+            "<compile>",
+            "eval_op"
+          );
+        } catch (error) {
+          // In blocking mode, onInspectionInterceptorResult would throw to block the operation
+          // To block the code generation, we need to return a string that will be used for the thrown error message
+          return (error as Error).message;
+        }
+      }
     });
   }
 }
