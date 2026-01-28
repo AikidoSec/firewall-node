8b50896902c38a851f10ee6cf8060df505033e89
Hans Ott <hansott@hotmail.be>
2026-01-28 17:51:16 +0100

    Remove test

--- library/sources/hono/getRawRequest.test.ts
+++
@@ -1,8 +0,0 @@
-import * as t from "tap";
-import { getRawNodeRequest } from "./getRawRequest";
-import type { Context as HonoContext } from "hono";
-
-t.test("returns undefined when c.env is undefined", async (t) => {
-  const mockContext = {} as HonoContext;
-  t.equal(getRawNodeRequest(mockContext), undefined);
-});
