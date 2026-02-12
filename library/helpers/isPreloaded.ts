628669777246ce23ab8f85864447d15835e975e2
Timo Kössler <info@timokoessler.de>
2026-02-12 09:59:09 +0100

    Also check NODE_OPTIONS env

--- library/helpers/isPreloaded.ts
+++ library/helpers/isPreloaded.ts
@@ -1,14 +1,38 @@
 /**
  * Checks whever the args include --require / -r or --import @aikidosec/firewall
+ * Also checks NODE_OPTIONS.
  */
 export function isPreloaded(): boolean {
-  for (const arg of process.execArgv) {
-    if (arg === "--require" || arg === "-r" || arg === "--import") {
-      const index = process.execArgv.indexOf(arg);
-      const moduleName = process.execArgv[index + 1];
-      if (moduleName && moduleName.includes("@aikidosec/firewall")) {
-        return true;
-      }
+  const processArgs = process.execArgv;
+  if (checkArgsForPreload(processArgs)) {
+    return true;
+  }
+
+  const nodeOptions = process.env.NODE_OPTIONS;
+  if (nodeOptions) {
+    const nodeOptionsArgs = nodeOptions.split(" ");
+    if (checkArgsForPreload(nodeOptionsArgs)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function checkArgsForPreload(args: string[]): boolean {
+  for (const arg of args) {
+    if (arg !== "--require" && arg !== "-r" && arg !== "--import") {
+      continue;
+    }
+
+    const index = args.indexOf(arg);
+    if (index === -1 || index === args.length - 1) {
+      continue;
+    }
+
+    const moduleName = args[index + 1];
+    if (moduleName && moduleName.includes("@aikidosec/firewall")) {
+      return true;
     }
   }
 
