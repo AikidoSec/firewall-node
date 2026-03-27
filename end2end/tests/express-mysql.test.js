d1afd97bb78de305ab4c2d739c86f60ca4002883
Hans Ott <hansott@hotmail.be>
2026-03-27 14:53:40 +0100

    Keep dry mode test in original position to reduce diff

--- end2end/tests/express-mysql.test.js
+++ end2end/tests/express-mysql.test.js
@@ -90,6 +90,72 @@ t.test("it blocks in blocking mode", (t) => {
     });
 });
 
+t.test("it does not block in dry mode", (t) => {
+  const server = spawn(`node`, [pathToApp, "4001"], {
+    env: { ...process.env, AIKIDO_DEBUG: "true" },
+  });
+
+  server.on("close", () => {
+    t.end();
+  });
+
+  let stdout = "";
+  server.stdout.on("data", (data) => {
+    stdout += data.toString();
+  });
+
+  let stderr = "";
+  server.stderr.on("data", (data) => {
+    stderr += data.toString();
+  });
+
+  // Wait for the server to start
+  timeout(2000)
+    .then(() =>
+      Promise.all([
+        fetch(
+          `http://localhost:4001/?petname=${encodeURIComponent("Njuska'); DELETE FROM cats;-- H")}`,
+          {
+            signal: AbortSignal.timeout(5000),
+          }
+        ),
+        fetch("http://localhost:4001/cats", {
+          signal: AbortSignal.timeout(5000),
+          method: "POST",
+          body: "<cat><name>Njuska'); DELETE FROM cats;-- H</name></cat>",
+          headers: {
+            "Content-Type": "application/xml",
+          },
+        }),
+        fetch("http://localhost:4001/?petname=Njuska", {
+          signal: AbortSignal.timeout(5000),
+        }),
+        fetch("http://localhost:4001/cats", {
+          signal: AbortSignal.timeout(5000),
+          method: "POST",
+          body: "<cat><name>Njuska</name></cat>",
+          headers: {
+            "Content-Type": "application/xml",
+          },
+        }),
+      ])
+    )
+    .then(([noSQLInjection, noSQLInjectionXml, normalSearch, normalAddXml]) => {
+      t.equal(noSQLInjection.status, 200);
+      t.equal(noSQLInjectionXml.status, 200);
+      t.equal(normalSearch.status, 200);
+      t.equal(normalAddXml.status, 200);
+      t.match(stdout, /Starting agent/);
+      t.notMatch(stderr, /Zen has blocked an SQL injection/);
+    })
+    .catch((error) => {
+      t.fail(error.message);
+    })
+    .finally(() => {
+      server.kill();
+    });
+});
+
 t.test("it blocks invalid SQL queries by default", (t) => {
   const server = spawn(`node`, [pathToApp, "4003"], {
     env: {
@@ -193,69 +259,3 @@ t.test(
       });
   }
 );
-
-t.test("it does not block in dry mode", (t) => {
-  const server = spawn(`node`, [pathToApp, "4001"], {
-    env: { ...process.env, AIKIDO_DEBUG: "true" },
-  });
-
-  server.on("close", () => {
-    t.end();
-  });
-
-  let stdout = "";
-  server.stdout.on("data", (data) => {
-    stdout += data.toString();
-  });
-
-  let stderr = "";
-  server.stderr.on("data", (data) => {
-    stderr += data.toString();
-  });
-
-  // Wait for the server to start
-  timeout(2000)
-    .then(() =>
-      Promise.all([
-        fetch(
-          `http://localhost:4001/?petname=${encodeURIComponent("Njuska'); DELETE FROM cats;-- H")}`,
-          {
-            signal: AbortSignal.timeout(5000),
-          }
-        ),
-        fetch("http://localhost:4001/cats", {
-          signal: AbortSignal.timeout(5000),
-          method: "POST",
-          body: "<cat><name>Njuska'); DELETE FROM cats;-- H</name></cat>",
-          headers: {
-            "Content-Type": "application/xml",
-          },
-        }),
-        fetch("http://localhost:4001/?petname=Njuska", {
-          signal: AbortSignal.timeout(5000),
-        }),
-        fetch("http://localhost:4001/cats", {
-          signal: AbortSignal.timeout(5000),
-          method: "POST",
-          body: "<cat><name>Njuska</name></cat>",
-          headers: {
-            "Content-Type": "application/xml",
-          },
-        }),
-      ])
-    )
-    .then(([noSQLInjection, noSQLInjectionXml, normalSearch, normalAddXml]) => {
-      t.equal(noSQLInjection.status, 200);
-      t.equal(noSQLInjectionXml.status, 200);
-      t.equal(normalSearch.status, 200);
-      t.equal(normalAddXml.status, 200);
-      t.match(stdout, /Starting agent/);
-      t.notMatch(stderr, /Zen has blocked an SQL injection/);
-    })
-    .catch((error) => {
-      t.fail(error.message);
-    })
-    .finally(() => {
-      server.kill();
-    });
-});
