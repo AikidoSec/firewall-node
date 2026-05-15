4b0ad0a1f42a4babece21d6a268e08830df20704
Timo Kössler <info@timokoessler.de>
2026-05-15 11:23:52 +0200

    Fix typo

--- library/helpers/clone.ts
+++ library/helpers/clone.ts
@@ -1,9 +1,9 @@
 // Node.js v16 does not have structuredClone, so we need to use a polyfill
-const clonneFunction =
+const cloneFunction =
   typeof structuredClone === "function"
     ? structuredClone
     : (obj: any) => JSON.parse(JSON.stringify(obj));
 
 export function clone<T>(obj: T): T {
-  return clonneFunction(obj);
+  return cloneFunction(obj);
 }
