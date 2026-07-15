c4e19405ddba4d0438e0459f3452ff8300c58ea5
Hans Ott <hansott@hotmail.be>
2026-07-15 18:27:26 +0200

    Minimize diff against main

--- library/helpers/tryParseURL.ts
+++ library/helpers/tryParseURL.ts
@@ -1,5 +1,11 @@
 import { getMajorNodeVersion } from "./getNodeVersion";
 
+type ParseFunction = (url: string) => URL | undefined;
+
+let tryParseURL: ParseFunction;
+
+tryParseURL = parseAndCatchError;
+
 function parseAndCatchError(url: string): URL | undefined {
   try {
     return new URL(url);
@@ -16,8 +22,10 @@ function canParse(url: string): URL | undefined {
   return undefined;
 }
 
+// URL.canParse(...) is a lot faster than using the constructor and catching the error
 // URL.canParse(...) can give wrong results on Node.js < 24, so we only use it from 24+.
-export const tryParseURL: (url: string) => URL | undefined =
-  typeof URL.canParse === "function" && getMajorNodeVersion() >= 24
-    ? canParse
-    : parseAndCatchError;
+if (typeof URL.canParse === "function" && getMajorNodeVersion() >= 24) {
+  tryParseURL = canParse;
+}
+
+export { tryParseURL };
