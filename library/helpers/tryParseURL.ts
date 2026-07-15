99ecdcf8a5ffcefd09af6f9198ce69bd9b73ce9c
Hans Ott <hansott@hotmail.be>
2026-07-15 18:20:29 +0200

    Use canParse on node +v24

--- library/helpers/tryParseURL.ts
+++ library/helpers/tryParseURL.ts
@@ -1,7 +1,23 @@
-export function tryParseURL(url: string): URL | undefined {
+import { getMajorNodeVersion } from "./getNodeVersion";
+
+function parseAndCatchError(url: string): URL | undefined {
   try {
     return new URL(url);
   } catch {
     return undefined;
   }
 }
+
+function canParse(url: string): URL | undefined {
+  if (URL.canParse(url)) {
+    return new URL(url);
+  }
+
+  return undefined;
+}
+
+// URL.canParse(...) can give wrong results on Node.js < 24, so we only use it from 24+.
+export const tryParseURL: (url: string) => URL | undefined =
+  typeof URL.canParse === "function" && getMajorNodeVersion() >= 24
+    ? canParse
+    : parseAndCatchError;
