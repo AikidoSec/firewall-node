0653e40b23f238e704535e557800f4277f936a24
Timo Kössler <info@timokoessler.de>
2026-08-20 12:45:22 +0200

    Add e2e test

---
+++ sample-apps/trpc-server/client/index.ts
@@ -0,0 +1,64 @@
+import {
+  createTRPCClient,
+  httpBatchStreamLink,
+  httpSubscriptionLink,
+  splitLink,
+} from "@trpc/client";
+
+import type { AppRouter } from "../server/index.js";
+import { deepEqual, rejects } from "assert/strict";
+
+const port = process.env.PORT || "4000";
+
+const trpc = createTRPCClient<AppRouter>({
+  links: [
+    splitLink({
+      condition: (op) => op.type === "subscription",
+      true: httpSubscriptionLink({
+        url: `http://localhost:${port}`,
+      }),
+      false: httpBatchStreamLink({
+        url: `http://localhost:${port}`,
+      }),
+    }),
+  ],
+});
+
+async function main() {
+  const expectBlocking = process.env.AIKIDO_BLOCK === "true";
+
+  await trpc.cats.reset.mutate();
+
+  const cats = await trpc.cats.list.query();
+
+  deepEqual(cats, [], "Cats should be empty initially");
+
+  const createdCat = await trpc.cats.create.mutate({ name: "miau" });
+
+  deepEqual(
+    createdCat,
+    { changes: 1, lastInsertRowid: 1 },
+    "Cat should be created"
+  );
+
+  const cat = await trpc.cats.byName.query("miau");
+  deepEqual(cat, { petname: "miau" }, "Cat should be found by name");
+
+  if (expectBlocking) {
+    await rejects(
+      () => trpc.cats.byName.query("test' OR '1'='1"),
+      "SQL injection should be blocked"
+    );
+  } else {
+    const injectedCat = await trpc.cats.byName.query("' OR '1'='1");
+    deepEqual(
+      injectedCat,
+      { petname: "miau" },
+      "SQL injection should return the first cat"
+    );
+  }
+
+  process.exit(0);
+}
+
+void main();
---
+++ sample-apps/trpc-server/server/index.ts
@@ -0,0 +1,45 @@
+/**
+ * This a minimal tRPC server
+ */
+import { createHTTPServer } from "@trpc/server/adapters/standalone";
+import { z } from "zod";
+import { db } from "./db.ts";
+import { publicProcedure, router } from "./trpc.ts";
+
+const appRouter = router({
+  cats: {
+    list: publicProcedure.query(async () => {
+      return db.prepare("SELECT * FROM cats").all();
+    }),
+    byName: publicProcedure.input(z.string()).query(async (opts) => {
+      const { input } = opts;
+      // This is intentionally vulnerable to SQL injection
+      return db.prepare(`SELECT * FROM cats WHERE petname = '${input}'`).get();
+    }),
+    create: publicProcedure
+      .input(z.object({ name: z.string() }))
+      .mutation(async (opts) => {
+        const { input } = opts;
+        return db
+          .prepare(`INSERT INTO cats (petname) VALUES ('${input.name}')`)
+          .run();
+      }),
+    reset: publicProcedure.mutation(async () => {
+      db.exec("DELETE FROM cats");
+      return { success: true };
+    }),
+  },
+});
+
+// Export type router type signature, this is used by the client.
+export type AppRouter = typeof appRouter;
+
+const server = createHTTPServer({
+  router: appRouter,
+});
+
+const port = process.env.PORT || "4000";
+
+server.listen(port, () => {
+  console.log(`tRPC server listening on http://localhost:${port}`);
+});
